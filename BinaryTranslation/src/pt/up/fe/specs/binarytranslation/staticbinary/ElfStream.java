7afac1edfdbcb1e80574491254bd48e34062e94a
JoaoBispo <joaobispo@gmail.com>
2019-09-19 18:32:17 +0100

    [BinaryTranslation] Adds InstructionStream.getType()

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/staticbinary/ElfStream.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/staticbinary/ElfStream.java
@@ -1,7 +1,12 @@
 package pt.up.fe.specs.binarytranslation.staticbinary;
 
 import pt.up.fe.specs.binarytranslation.InstructionStream;
+import pt.up.fe.specs.binarytranslation.InstructionStreamType;
 
 public interface ElfStream extends InstructionStream {
 
+    @Override
+    default InstructionStreamType getType() {
+        return InstructionStreamType.STATIC_ELF;
+    }
 }
