4a8eb93709e579a53295399c709d284d511348bc
JoaoBispo <joaobispo@gmail.com>
2019-09-24 12:49:37 +0100

    [ARM] Renames unit test to Tester, to disable test in Jenkins

--- ARM/test/org/specs/Arm/asm/ArmElfStreamTester.java
+++ ARM/test/org/specs/Arm/asm/ArmElfStreamTester.java
@@ -1,20 +1,16 @@
 package org.specs.Arm.asm;
 
 import java.io.File;
-import java.util.Arrays;
 
 import org.junit.Test;
 
 import pt.up.fe.specs.binarytranslation.Instruction;
 import pt.up.fe.specs.util.SpecsIo;
-import pt.up.fe.specs.util.SpecsSystem;
 
-public class ArmElfStreamTest {
+public class ArmElfStreamTester {
 
     @Test
     public void test() {
-
-        SpecsSystem.runProcess(Arrays.asList("dir"), true, true);
 
         File fd = SpecsIo.resourceCopy("org/specs/Arm/asm/test/test.elf");
         fd.deleteOnExit();
