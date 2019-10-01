6c045336d1153d6a87158a1f627f7741c29b0b1c
JoaoBispo <joaobispo@gmail.com>
2019-10-01 01:09:10 +0100

    [ARM] Moves ARM Isa parser test to file that Jenkins can test

--- ARM/test/org/specs/Arm/asm/ArmElfStreamTester.java
+++ ARM/test/org/specs/Arm/asm/ArmElfStreamTester.java
@@ -1,16 +1,11 @@
 package org.specs.Arm.asm;
 
 import java.io.File;
-import java.math.BigInteger;
 
 import org.junit.Test;
-import org.specs.Arm.asmparser.ArmInstructionParsers;
 
 import pt.up.fe.specs.binarytranslation.Instruction;
-import pt.up.fe.specs.binarytranslation.asmparser.IsaParser;
-import pt.up.fe.specs.binarytranslation.generic.GenericInstruction;
 import pt.up.fe.specs.util.SpecsIo;
-import pt.up.fe.specs.util.utilities.LineStream;
 
 public class ArmElfStreamTester {
 
@@ -24,24 +19,6 @@ public class ArmElfStreamTester {
             Instruction inst = null;
             while ((inst = el.nextInstruction()) != null) {
                 System.out.println(inst);
-            }
-        }
-
-    }
-
-    @Test
-    public void testArmParser() {
-        IsaParser parser = ArmInstructionParsers.getArmIsaParser();
-
-        try (var lines = LineStream.newInstance(SpecsIo.resourceToStream("org/specs/Arm/asm/test/asm.txt"), "");) {
-            while (lines.hasNextLine()) {
-                String asmInstruction = lines.nextLine();
-                System.out.println("LINE: " + asmInstruction);
-
-                var inst = new GenericInstruction(0, new BigInteger(asmInstruction, 16));
-                System.out.println("INST: " + inst.toBinaryString());
-                var data = parser.parse(inst);
-                System.out.println("DATA: " + data);
             }
         }
 
---
+++ ARM/test/org/specs/Arm/asm/ArmTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2019 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package org.specs.Arm.asm;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+import org.specs.Arm.asmparser.ArmInstructionParsers;
+
+import pt.up.fe.specs.binarytranslation.asmparser.IsaParser;
+import pt.up.fe.specs.binarytranslation.generic.GenericInstruction;
+import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsStrings;
+import pt.up.fe.specs.util.utilities.LineStream;
+
+public class ArmTest {
+
+    @Test
+    public void testArmParser() {
+        IsaParser parser = ArmInstructionParsers.getArmIsaParser();
+
+        StringBuilder output = new StringBuilder();
+        try (var lines = LineStream.newInstance(SpecsIo.resourceToStream("org/specs/Arm/asm/test/asm.txt"), "");) {
+            while (lines.hasNextLine()) {
+                String asmInstruction = lines.nextLine();
+
+                var inst = new GenericInstruction(0, new BigInteger(asmInstruction, 16));
+                var data = parser.parse(inst);
+                output.append(data).append("\n");
+            }
+        }
+
+        String expected = SpecsStrings
+                .normalizeFileContents(SpecsIo.getResource("org/specs/Arm/asm/test/arm_parser.result"), true);
+        String current = SpecsStrings.normalizeFileContents(output.toString(), true);
+
+        assertEquals(expected, current);
+    }
+
+}
