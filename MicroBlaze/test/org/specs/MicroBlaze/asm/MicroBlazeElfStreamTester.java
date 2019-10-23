6fb6440f7d6a3c46fa7c9928beb15a441f09b2de
nmcp88 <nmcp88@gmail.com>
2019-10-23 15:36:43 +0100

    [MicroBlaze] implemented getOperands for MicroBlazeAsmFields, now fields are used to initialized operands with the correct mode (e.g., register_write)

--- MicroBlaze/test/org/specs/MicroBlaze/asm/MicroBlazeElfStreamTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/asm/MicroBlazeElfStreamTester.java
@@ -11,7 +11,7 @@ public class MicroBlazeElfStreamTester {
 
     @Test
     public void test() {
-        File fd = SpecsIo.resourceCopy("org/specs/MicroBlaze/asm/test/matmul_n4096_l1000.elf");
+        File fd = SpecsIo.resourceCopy("org/specs/MicroBlaze/asm/test/test.elf");
         fd.deleteOnExit();
 
         try (MicroBlazeElfStream el = new MicroBlazeElfStream(fd)) {
