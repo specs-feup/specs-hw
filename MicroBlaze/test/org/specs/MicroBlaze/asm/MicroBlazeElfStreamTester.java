8a608b5417516b73965cd896eb70c605b1f2cf5f
nmcp88 <nmcp88@gmail.com>
2019-10-25 13:20:06 +0100

    [MicroBlaze] added tester for verilog module generation for microblaze

--- MicroBlaze/test/org/specs/MicroBlaze/asm/MicroBlazeElfStreamTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/asm/MicroBlazeElfStreamTester.java
@@ -12,7 +12,7 @@ public class MicroBlazeElfStreamTester {
 
     @Test
     public void test() {
-        File fd = SpecsIo.resourceCopy("org/specs/MicroBlaze/asm/test/dump.txt");
+        File fd = SpecsIo.resourceCopy("org/specs/MicroBlaze/asm/test/test.elf");
         fd.deleteOnExit();
 
         try (MicroBlazeElfStream el = new MicroBlazeElfStream(fd)) {
