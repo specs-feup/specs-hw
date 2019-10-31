398484b45df7d0f1c2bca05fe32462cfa026573f
nmcp88 <nmcp88@gmail.com>
2019-10-31 15:07:29 +0000

    [BinaryTranslation] added automatic verification of operand bitwidth to abstract operand class

--- BinaryTranslation/test/pt/up/fe/specs/binarytranslation/test/ExpressionOperatorTest.java
+++ BinaryTranslation/test/pt/up/fe/specs/binarytranslation/test/ExpressionOperatorTest.java
@@ -5,10 +5,9 @@ import org.junit.Test;
 import pt.up.fe.specs.binarytranslation.expression.ExpressionOperator;
 
 public class ExpressionOperatorTest {
-/*
+
     @Test
     public void test() {
-        System.out.print(ExpressionOperator.add.apply("ra", "rb"));
+        System.out.print(ExpressionOperator.equals.apply("ra", "rb"));
     }
-*/
 }
