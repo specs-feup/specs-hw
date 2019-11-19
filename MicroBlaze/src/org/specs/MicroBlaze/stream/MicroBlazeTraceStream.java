05b2d2191d9f26b7fcf0bb19ac8f45404f719fa2
nmcp88 <nmcp88@gmail.com>
2019-11-19 14:05:25 +0000

    [MicroBlaze] first barebones working version of MicroBlazeTraceStream using gdb and qemu in the backend

--- MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceStream.java
+++ MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceStream.java
@@ -1,108 +1,73 @@
 package org.specs.MicroBlaze.stream;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import org.specs.MicroBlaze.instruction.MicroBlazeInstruction;
 
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
+import pt.up.fe.specs.util.SpecsStrings;
+import pt.up.fe.specs.util.SpecsSystem;
+import pt.up.fe.specs.util.utilities.LineStream;
 
 public class MicroBlazeTraceStream implements InstructionStream {
 
-    private BufferedReader gdbout;
-    private BufferedWriter gdbin;
-    private Process gdb;
-    private Process qemu;
+    private static final Pattern MB_REGEX = Pattern.compile("0x([0-9a-f]+)\\s<.*>:\\s0x([0-9a-f]+)");
+    private LineStream insts;
 
     public MicroBlazeTraceStream(File elfname) {
 
-        // 1. make the .gdbinit file
+        // 1. make the gdb script file
         try {
-            PrintWriter gdbinit = new PrintWriter(".gdbinit");
-            gdbinit.println("target remote localhost:1234");
-            gdbinit.println("set logging on");
-            gdbinit.println("set logging file " + elfname.getName() + ".log");
+            String elfpath = elfname.getAbsolutePath();
+            String dtbfullpath = "./resources/org/specs/MicroBlaze/qemu/system.dtb";
+
+            PrintWriter gdbinit = new PrintWriter("tmpscript.gdb");
+            gdbinit.println("set confirm off");
+            gdbinit.println("undisplay");
+            gdbinit.println("set print address off");
             gdbinit.println("set height 0");
+            gdbinit.println("file " + elfpath);
+            gdbinit.println(
+                    "target remote | /media/nuno/HDD/work/projects/qemu/microblazeel-softmmu/qemu-system-microblazeel "
+                            + "-M microblaze-fdt-plnx -m 64 -device loader,file=" + elfpath + " -gdb stdio "
+                            + "-hw-dtb " + dtbfullpath + " -display none -S");
+            gdbinit.println("while $pc != 0x80");
+            gdbinit.println("stepi");
+            gdbinit.println("x/x $pc");
+            gdbinit.println("end");
+            gdbinit.println("kill");
+            gdbinit.println("quit");
             gdbinit.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 2. launch processes
-        try {
-            // convert elf to bin
-            var objcpypb = new ProcessBuilder(
-                    Arrays.asList("mb-objcopy", "-O", "binary", elfname.getAbsolutePath(), "test.bin"));
-            var objcopy = objcpypb.start();
-            try {
-                objcopy.waitFor();
-                objcopy.destroy();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            // launch QEMU for Microblaze
-            var qemupb = new ProcessBuilder(Arrays.asList("qemu-system-microblaze",
-                    "-machine petalogix-ml605", "-m 256", "-nographic", "-s", "-S", "-kernel", "test.bin"));
-            qemu = qemupb.start();
-
-            // launch gdb
-            var gdbpb = new ProcessBuilder(Arrays.asList("mb-gdb"));
-            gdb = gdbpb.start();
-            gdbout = new BufferedReader((new InputStreamReader(gdb.getInputStream())));
-            gdbin = new BufferedWriter((new OutputStreamWriter(gdb.getOutputStream())));
-
-            // read all starting garbage from gdb
-            while (!gdbout.ready())
-                ;
-
-            while (gdbout.ready())
-                System.out.print(gdbout.readLine() + "\n");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        var output = SpecsSystem.runProcess(
+                Arrays.asList("mb-gdb", "-x", "tmpscript.gdb"), new File("."), true, false);
+        insts = LineStream.newInstance(output.getStdOut());
     }
 
     @Override
     public Instruction nextInstruction() {
 
-        // instruct to step and disassemble
-        try {
-            gdbin.write("disas/r $pc, $pc+1");
-            gdbin.flush();
+        String line = null;
+        while (((line = insts.nextLine()) != null) && !SpecsStrings.matches(line, MB_REGEX))
+            ;
 
-            gdbin.write("stepi");
-            gdbin.flush();
-
-            /*
-            var ln1 = gdbout.readLine();
-            var ln2 = gdbout.readLine();
-            var ln3 = gdbout.readLine();
-            
-            System.out.print(ln1);
-            System.out.print(ln2);
-            System.out.print(ln3);
-            */
-
-            // read all starting garbage from gdb
-            while (!gdbout.ready())
-                ;
-
-            while (gdbout.ready())
-                System.out.print(gdbout.readLine() + "\n");
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (line == null) {
+            return null;
         }
 
-        return null;
+        var addressAndInst = SpecsStrings.getRegex(line, MB_REGEX);
+        var addr = addressAndInst.get(0).trim();
+        var inst = addressAndInst.get(1).trim();
+        return MicroBlazeInstruction.newInstance(addr, inst);
     }
 
     @Override
@@ -112,20 +77,12 @@ public class MicroBlazeTraceStream implements InstructionStream {
 
     @Override
     public void close() {
-        qemu.destroy();
-        gdb.destroy();
-
-        try {
-            gdbin.close();
-            gdbout.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        insts.close();
     }
 
     @Override
     public int getInstructionWidth() {
         return 4; // return in bytes
+        // TODO replace this with something smarter
     }
 }
--- MicroBlaze/test/org/specs/MicroBlaze/test/MicroBlazeTraceStreamTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/MicroBlazeTraceStreamTester.java
@@ -5,18 +5,21 @@ import java.io.File;
 import org.junit.Test;
 import org.specs.MicroBlaze.stream.MicroBlazeTraceStream;
 
+import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.util.SpecsIo;
 
 public class MicroBlazeTraceStreamTester {
 
     @Test
     public void test() {
-        File fd = SpecsIo.resourceCopy("./resources/org/specs/MicroBlaze/asm/test/test.elf");
+        File fd = SpecsIo.resourceCopy("org/specs/MicroBlaze/asm/test/helloworld.elf");
         fd.deleteOnExit();
 
         try (MicroBlazeTraceStream el = new MicroBlazeTraceStream(fd)) {
-
+            Instruction inst = null;
+            while ((inst = el.nextInstruction()) != null) {
+                inst.printInstruction();
+            }
         }
     }
-
 }
