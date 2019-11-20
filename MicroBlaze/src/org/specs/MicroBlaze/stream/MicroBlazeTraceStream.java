132d75762ef4a4ad68de146724041b78992ba365
nmcp88 <nmcp88@gmail.com>
2019-11-20 16:29:56 +0000

    Added resources to test the trace streaming for microblaze and arm

--- MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceStream.java
+++ MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceStream.java
@@ -9,12 +9,13 @@ import java.util.regex.Pattern;
 import org.specs.MicroBlaze.instruction.MicroBlazeInstruction;
 
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
-import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
+import pt.up.fe.specs.binarytranslation.stream.TraceInstructionStream;
 import pt.up.fe.specs.util.SpecsStrings;
 import pt.up.fe.specs.util.SpecsSystem;
+import pt.up.fe.specs.util.asm.processor.RegisterTable;
 import pt.up.fe.specs.util.utilities.LineStream;
 
-public class MicroBlazeTraceStream implements InstructionStream {
+public class MicroBlazeTraceStream implements TraceInstructionStream {
 
     private static final Pattern MB_REGEX = Pattern.compile("0x([0-9a-f]+)\\s<.*>:\\s0x([0-9a-f]+)");
     private LineStream insts;
@@ -37,7 +38,7 @@ public class MicroBlazeTraceStream implements InstructionStream {
                             + "-M microblaze-fdt-plnx -m 64 -device loader,file=" + elfpath + " -gdb stdio "
                             + "-hw-dtb " + dtbfullpath + " -display none -S");
             gdbinit.println("while $pc != 0x80");
-            gdbinit.println("stepi");
+            gdbinit.println("stepi 1");
             gdbinit.println("x/x $pc");
             gdbinit.println("end");
             gdbinit.println("kill");
@@ -51,6 +52,13 @@ public class MicroBlazeTraceStream implements InstructionStream {
         var output = SpecsSystem.runProcess(
                 Arrays.asList("mb-gdb", "-x", "tmpscript.gdb"), new File("."), true, false);
         insts = LineStream.newInstance(output.getStdOut());
+    }
+
+    public void rawDump() {
+        String line = null;
+        while ((line = insts.nextLine()) != null) {
+            System.out.print(line + "\n");
+        }
     }
 
     @Override
@@ -71,11 +79,6 @@ public class MicroBlazeTraceStream implements InstructionStream {
     }
 
     @Override
-    public InstructionStreamType getType() {
-        return InstructionStreamType.TRACE;
-    }
-
-    @Override
     public void close() {
         insts.close();
     }
@@ -84,5 +87,29 @@ public class MicroBlazeTraceStream implements InstructionStream {
     public int getInstructionWidth() {
         return 4; // return in bytes
         // TODO replace this with something smarter
+    }
+
+    @Override
+    public Instruction nextTraceInstruction() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getNumInstructions() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public RegisterTable getRegisters() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getCycles() {
+        // TODO Auto-generated method stub
+        return 0;
     }
 }
