3798daa2215cbb2418e743143cb7be141982ec6c
JoaoBispo <joaobispo@gmail.com>
2019-11-21 23:45:20 +0000

    [ARM] Refactored ArmTraceStream to wait for an error before proceeding

--- ARM/src/org/specs/Arm/stream/ArmTraceStream.java
+++ ARM/src/org/specs/Arm/stream/ArmTraceStream.java
@@ -3,7 +3,6 @@ package org.specs.Arm.stream;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.regex.Pattern;
@@ -25,37 +24,69 @@ import pt.up.fe.specs.util.utilities.Replacer;
 
 public class ArmTraceStream implements TraceInstructionStream {
 
+    private static final String GDB_EXE = "aarch64-none-elf-gdb";
+    private static final String QEMU_EXE = "qemu-system-aarch64";
+
     private static final Pattern REGEX = Pattern.compile("0x([0-9a-f]+)\\s<.*>:\\s0x([0-9a-f]+)");
     private final LineStream insts;
 
     private static ArmTraceStream newInstance(File elfname) {
 
         ConcurrentChannel<LineStream> lineStreamChannel = new ConcurrentChannel<>(1);
-        ConcurrentChannel<Exception> exceptionChannel = new ConcurrentChannel<>(1);
 
         String elfpath = elfname.getAbsolutePath();
         var gdbScript = new Replacer(ArmResource.QEMU_AARCH64_GDB_TEMPLATE);
         gdbScript.replace("<ELFNAME>", elfpath);
-        gdbScript.replace("<QEMUBIN>", "/usr/bin/qemu-system-aarch64");
+        gdbScript.replace("<QEMUBIN>", "/usr/bin/" + QEMU_EXE);
         SpecsIo.write(new File("tmpscript.gdb"), gdbScript.toString());
 
-        var executor = Executors.newSingleThreadScheduledExecutor();
-        executor.execute(() -> ArmTraceStream.runSimulator(lineStreamChannel, exceptionChannel));
-        executor.shutdown();
+        var futureSimulatorError = SpecsSystem.getFuture(() -> ArmTraceStream.runSimulator(lineStreamChannel));
 
+        // Wait for a second, to check if there is an exception when launching the simulator
+        var error = SpecsSystem.get(futureSimulatorError, 1, TimeUnit.SECONDS);
+
+        if (error != null) {
+            throw new RuntimeException(
+                    "Could not properly launch the simulator", error);
+        }
+
+        // No error detected, obtain LineStream
         LineStream insts = null;
-
         try {
-            insts = lineStreamChannel.createConsumer().poll(2, TimeUnit.SECONDS);
+            insts = lineStreamChannel.createConsumer().poll(1, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
         if (insts == null) {
-            var gdbException = exceptionChannel.createConsumer().poll();
-            throw new RuntimeException("Could not launch simulator, timed-out after 2 seconds", gdbException);
+            throw new RuntimeException("Could not obtain output stream of GDB");
         }
-
+        /*
+        var executor = Executors.newSingleThreadScheduledExecutor();
+        ConcurrentChannel<Exception> exceptionChannel = new ConcurrentChannel<>(1);
+        
+        executor.execute(() -> ArmTraceStream.runSimulator(lineStreamChannel, exceptionChannel));
+        executor.shutdown();
+        
+        // lineStreamChannel can get populated with a stream even if there is a problem with the process
+        // check
+        LineStream insts = null;
+        
+        try {
+            insts = lineStreamChannel.createConsumer().poll(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        
+        if (insts == null) {
+            // Try to get the exception, if there is none in the channel it's ok
+            var gdbException = exceptionChannel.createConsumer().poll();
+            throw new RuntimeException(
+                    "Could not launch simulator, timed-out after 2 seconds. Please check if GDB (" + GDB_EXE
+                            + ") and QEMU (" + QEMU_EXE + ")  are correctly installed",
+                    gdbException);
+        }
+        */
         return new ArmTraceStream(insts);
     }
 
@@ -65,6 +96,32 @@ public class ArmTraceStream implements TraceInstructionStream {
 
     public ArmTraceStream(File elfname) {
         this(newInstance(elfname).insts);
+    }
+
+    private static Exception runSimulator(ConcurrentChannel<LineStream> lineStreamChannel) {
+
+        Function<InputStream, Boolean> stdout = inputStream -> {
+            lineStreamChannel.createProducer().offer(LineStream.newInstance(inputStream, "gdb_stdout"));
+            return true;
+        };
+
+        Function<InputStream, String> stderr = new StreamToString(false, true, OutputType.StdErr);
+
+        try {
+            var command = Arrays.asList(GDB_EXE, "-x", "tmpscript.gdb");
+            var output = SpecsSystem.runProcess(command, new File("."), stdout, stderr);
+
+            // Throw an exception of any error is present
+            var err = output.getStdErr();
+            if (!err.isEmpty()) {
+                return new RuntimeException("Problems while running GDB:\n" + err);
+            }
+
+            return null;
+
+        } catch (Exception e) {
+            return e;
+        }
     }
 
     private static void runSimulator(ConcurrentChannel<LineStream> lineStreamChannel,
@@ -78,12 +135,17 @@ public class ArmTraceStream implements TraceInstructionStream {
         Function<InputStream, String> stderr = new StreamToString(false, true, OutputType.StdErr);
 
         try {
-            SpecsSystem.runProcess(Arrays.asList("aarch64-none-elf-gdb2",
-                    "-x", "tmpscript.gdb"), new File("."), stdout, stderr);
+            var command = Arrays.asList(GDB_EXE, "-x", "tmpscript.gdb");
+            var output = SpecsSystem.runProcess(command, new File("."), stdout, stderr);
+
+            // Throw an exception of any error is present
+            var err = output.getStdErr();
+            if (!err.isEmpty()) {
+                throw new RuntimeException("Problems while running GDB:\n" + err);
+            }
 
         } catch (Exception e) {
             exceptionChannel.createProducer().offer(e);
-            // e.printStackTrace();
         }
     }
 
