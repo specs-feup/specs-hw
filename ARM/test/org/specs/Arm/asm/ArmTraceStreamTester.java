08f2554d4c5b1dd81ec70288822dbcf33e0ca097
nmcp88 <nmcp88@gmail.com>
2019-11-21 11:11:53 +0000

    Added resources for ARM, wrote ArmTraceInstructionStream, and its tester; its working, under certain compilation conditions for the ELFs

---
+++ ARM/test/org/specs/Arm/asm/ArmTraceStreamTester.java
@@ -0,0 +1,27 @@
+package org.specs.Arm.asm;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.specs.Arm.stream.ArmTraceStream;
+
+import pt.up.fe.specs.binarytranslation.instruction.Instruction;
+import pt.up.fe.specs.util.SpecsIo;
+
+public class ArmTraceStreamTester {
+
+    @Test
+    public void test() {
+        File fd = SpecsIo.resourceCopy("org/specs/Arm/asm/aarch64-bare-metal-qemu/test64.elf");
+        fd.deleteOnExit();
+
+        try (ArmTraceStream el = new ArmTraceStream(fd)) {
+            Instruction inst = null;
+            while ((inst = el.nextInstruction()) != null) {
+                inst.printInstruction();
+            }
+            // el.rawDump();
+        }
+    }
+
+}
