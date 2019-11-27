f5ce35e7ded4466265d86cd972d3a69d5f54651e
nmcp88 <nmcp88@gmail.com>
2019-11-27 12:46:46 +0000

    [BinaryTranslation] refactored ATraceInstructionStream to not use SpecsSystem

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/stream/ATraceInstructionStream.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/stream/ATraceInstructionStream.java
@@ -1,29 +1,25 @@
 package pt.up.fe.specs.binarytranslation.stream;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 import pt.up.fe.specs.util.SpecsIo;
-import pt.up.fe.specs.util.SpecsSystem;
 import pt.up.fe.specs.util.asm.processor.RegisterTable;
 import pt.up.fe.specs.util.collections.concurrentchannel.ConcurrentChannel;
 import pt.up.fe.specs.util.providers.ResourceProvider;
-import pt.up.fe.specs.util.system.OutputType;
-import pt.up.fe.specs.util.system.StreamToString;
 import pt.up.fe.specs.util.utilities.LineStream;
 import pt.up.fe.specs.util.utilities.Replacer;
 
 public abstract class ATraceInstructionStream implements TraceInstructionStream {
 
+    private final Process gdb;
     protected final LineStream insts;
 
-    protected ATraceInstructionStream(File elfname, ResourceProvider gdbtmpl,
+    protected static Process newSimulator(File elfname, ResourceProvider gdbtmpl,
             String gdbexe, String qemuexe) {
-
-        ConcurrentChannel<LineStream> lineStreamChannel = new ConcurrentChannel<>(1);
 
         String elfpath = elfname.getAbsolutePath();
         var gdbScript = new Replacer(gdbtmpl);
@@ -31,21 +27,30 @@ public abstract class ATraceInstructionStream implements TraceInstructionStream 
         gdbScript.replace("<QEMUBIN>", "/usr/bin/" + qemuexe);
         SpecsIo.write(new File("tmpscript.gdb"), gdbScript.toString());
 
-        var futureSimulatorError = SpecsSystem
-                .getFuture(() -> ATraceInstructionStream.runSimulator(lineStreamChannel, gdbexe));
+        // start gdb
+        Process gdb = null;
+        try {
+            ProcessBuilder builder = new ProcessBuilder(Arrays.asList(gdbexe, "-x", "tmpscript.gdb"));
+            builder.directory(new File("."));
+            gdb = builder.start();
 
-        // Wait for a second, to check if there is an exception when launching the simulator
-        var error = SpecsSystem.get(futureSimulatorError, 1, TimeUnit.SECONDS);
-
-        if (error != null) {
-            throw new RuntimeException(
-                    "Could not properly launch the simulator", error);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not run GDB bin with name: " + gdbexe);
         }
+
+        return gdb;
+    }
+
+    protected static LineStream newLineStream(Process gdb) {
 
         // No error detected, obtain LineStream
         LineStream insts = null;
         try {
+            ConcurrentChannel<LineStream> lineStreamChannel = new ConcurrentChannel<>(1);
+            InputStream inputStream = gdb.getInputStream();
+            lineStreamChannel.createProducer().offer(LineStream.newInstance(inputStream, "gdb_stdout"));
             insts = lineStreamChannel.createConsumer().poll(1, TimeUnit.SECONDS);
+
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -54,33 +59,13 @@ public abstract class ATraceInstructionStream implements TraceInstructionStream 
             throw new RuntimeException("Could not obtain output stream of GDB");
         }
 
-        this.insts = insts;
+        return insts;
     }
 
-    private static Exception runSimulator(ConcurrentChannel<LineStream> lineStreamChannel, String gdbexe) {
-
-        Function<InputStream, Boolean> stdout = inputStream -> {
-            lineStreamChannel.createProducer().offer(LineStream.newInstance(inputStream, "gdb_stdout"));
-            return true;
-        };
-
-        Function<InputStream, String> stderr = new StreamToString(false, true, OutputType.StdErr);
-
-        try {
-            var command = Arrays.asList(gdbexe, "-x", "tmpscript.gdb");
-            var output = SpecsSystem.runProcess(command, new File("."), stdout, stderr);
-
-            // Throw an exception of any error is present
-            var err = output.getStdErr();
-            if (!err.isEmpty()) {
-                return new RuntimeException("Problems while running GDB:\n" + err);
-            }
-
-            return null;
-
-        } catch (Exception e) {
-            return e;
-        }
+    protected ATraceInstructionStream(File elfname, ResourceProvider gdbtmpl,
+            String gdbexe, String qemuexe) {
+        this.gdb = ATraceInstructionStream.newSimulator(elfname, gdbtmpl, gdbexe, qemuexe);
+        this.insts = ATraceInstructionStream.newLineStream(gdb);
     }
 
     public void rawDump() {
@@ -106,6 +91,14 @@ public abstract class ATraceInstructionStream implements TraceInstructionStream 
     }
 
     public void close() {
+
+        // wait for gdb to die before closing everything
+        try {
+            gdb.waitFor();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         insts.close();
     }
 }
