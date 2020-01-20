7c345053a0e179591b95a7298c589a9665845723
nmcp88 <nmcp88@gmail.com>
2020-01-20 16:52:42 +0000

    [BinaryTranslation] rudimentary dotty printing for purposes of debug of graph generation

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/graphs/BinarySegmentGraph.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/graphs/BinarySegmentGraph.java
@@ -21,6 +21,13 @@ import java.util.Map;
 import pt.up.fe.specs.binarytranslation.binarysegments.BinarySegment;
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 
+/**
+ * This class represents a binary segment as a graph, by hosting the instructions of the segment as the data transported
+ * by graph nodes
+ * 
+ * @author nuno
+ *
+ */
 public class BinarySegmentGraph {
 
     private int numnodes;
@@ -39,30 +46,98 @@ public class BinarySegmentGraph {
     /*
      * Map input registers to nodes which have written over them?
      */
-    private Map<String, GraphNode> lastwritter;
+    private Map<String, GraphNode> lastwriter;
 
     public BinarySegmentGraph(BinarySegment seg) {
+        this.numloads = 0;
+        this.numstores = 0;
         this.seg = seg;
         this.nodes = new ArrayList<GraphNode>();
 
         // get symbolic instruction list
+        int nodecounter = 0;
         var insts = seg.getInstructions();
         for (Instruction i : insts) {
 
             // add all instructions to node list, convert instruction to proper node
             // also converts instruction AOperands into GraphInputs (TODO: conversion is still incomplete)
-            this.nodes.add(new GraphNode(i));
+            var node = new GraphNode(i);
+            node.setNodenr(nodecounter++);
+            this.nodes.add(node);
         }
 
+        // create empty lists for adjacency table
+        this.adjacencyTable = new HashMap<GraphNode, List<GraphNode>>();
+        for (GraphNode n : this.nodes) {
+            this.adjacencyTable.put(n, new ArrayList<GraphNode>());
+        }
+
+        // bubble sort?
         // 1. analyze nodes in order of insertion
         // mark registers which have been written too?
-        this.lastwritter = new HashMap<String, GraphNode>();
+        this.lastwriter = new HashMap<String, GraphNode>();
         for (GraphNode n : this.nodes) {
-            // for(GraphInput in : n.getInputs()) {
-            // ???
-            // }
+
+            // replace this nodes inputs with previous outputs, if any
+            for (GraphInput in : n.getInputs()) {
+                var inputval = in.getValue();
+                if (lastwriter.containsKey(inputval)) {
+
+                    // producer node ID
+                    var producer = lastwriter.get(inputval);
+                    var value = producer.getNodeNr();
+                    in.setInputAs(GraphInputType.noderesult, Integer.toString(value));
+
+                    // set level
+                    if (n.getLevel() <= producer.getLevel())
+                        n.setLevel(producer.getLevel() + 1);
+
+                    // update adjacency table
+                    this.adjacencyTable.get(n).add(producer);
+                }
+            }
+
+            // update lastwriter table
+            for (GraphOutput out : n.getOutputs()) {
+                lastwriter.put(out.getValue(), n);
+            }
+        }
+
+        // count final stuff
+        // e.g. numstores, numloads
+        for (GraphNode n : this.nodes) {
+
+            if (n.getInst().isLoad())
+                this.numloads++;
+
+            else if (n.getInst().isStore())
+                this.numstores++;
         }
 
         // compute CPL, and ILP, and expected speedup (depends on segment type and target hw?)
+    }
+
+    /*
+     * Tester function to print this graph as a dotty, into the console
+     */
+    public void printDotty() {
+
+        System.out.print("digraph G {\n");
+        for (GraphNode n : this.adjacencyTable.keySet()) {
+
+            // connections to self from register inputs
+            for (GraphInput in : n.getInputs()) {
+                if (in.getType() != GraphInputType.noderesult) {
+                    System.out.print("\t" + in.getValue() + " -> " + n.getRepresentation() + ";\n");
+                }
+            }
+
+            // connections to children
+            var children = this.adjacencyTable.get(n);
+            for (GraphNode c : children) {
+                System.out.print("\t" + n.getRepresentation() + " -> " + c.getRepresentation() + ";\n");
+            }
+        }
+        System.out.print("}\n");
     }
 }
---
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/graphs/GraphOutput.java
@@ -0,0 +1,39 @@
+package pt.up.fe.specs.binarytranslation.graphs;
+
+import pt.up.fe.specs.binarytranslation.instruction.Operand;
+
+public class GraphOutput {
+
+    private final GraphOutputType type;
+    private final String value;
+    // bitwidth?
+
+    public GraphOutput(GraphOutputType tp, String value) {
+        this.type = tp;
+        this.value = value;
+    }
+
+    public GraphOutput(Operand op) {
+
+        // TODO Complete this conversion to account for datawidths
+
+        this.value = op.getStringValue();
+
+        // TODO how to determine is operand is a data or control output? would need instruction, or to add a field to
+
+        // TODO actually, the branch oeprations of the microblaze have NO output register
+
+        // OperandTypes
+        // if(op)
+
+        this.type = GraphOutputType.data;
+    }
+
+    public GraphOutputType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
