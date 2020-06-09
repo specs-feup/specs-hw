cc3dda3d109cddf63ee9f6c833878cf4695eea74
nmcp88 <nmcp88@gmail.com>
2020-06-09 18:00:29 +0100

    [MicroBlaze] added tester for CustomInstructionUnit generation

--- MicroBlaze/test/org/specs/MicroBlaze/test/MicroBlazeVerilogModuleGeneratorTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/MicroBlazeVerilogModuleGeneratorTester.java
@@ -8,31 +8,30 @@ import org.specs.MicroBlaze.stream.MicroBlazeElfStream;
 
 import pt.up.fe.specs.binarytranslation.binarysegments.BinarySegment;
 import pt.up.fe.specs.binarytranslation.binarysegments.detection.FrequentStaticSequenceDetector;
-import pt.up.fe.specs.binarytranslation.hardwaregeneration.VerilogModuleGenerator;
+import pt.up.fe.specs.binarytranslation.hardware.custominstruction.CustomInstructionUnitGenerator;
 import pt.up.fe.specs.util.SpecsIo;
 
 public class MicroBlazeVerilogModuleGeneratorTester {
-
+    /*
     private List<BinarySegment> getSequences() {
         File fd = SpecsIo.resourceCopy("org/specs/MicroBlaze/asm/test/test.elf");
         fd.deleteOnExit();
-
+    
         List<BinarySegment> bblist = null;
         try (MicroBlazeElfStream el = new MicroBlazeElfStream(fd)) {
             var bbd = new FrequentStaticSequenceDetector(el);
             var bundle = bbd.detectSegments();
             bblist = bundle.getSegments();
-
+    
         }
         return bblist;
     }
-
+    
     @Test
     public void test() {
         List<BinarySegment> bblist = getSequences();
-        VerilogModuleGenerator gen1 = new VerilogModuleGenerator();
+        CustomInstructionUnitGenerator gen1 = new CustomInstructionUnitGenerator();
         gen1.generateHardware(bblist.get(0));
         return;
-    }
-
+    }*/
 }
