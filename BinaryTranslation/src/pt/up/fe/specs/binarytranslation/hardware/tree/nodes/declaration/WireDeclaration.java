45ed4ba240e3a44a91d0c56b4713ce4c1d788b75
nmcp88 <nmcp88@gmail.com>
2020-06-29 08:36:51 +0100

    [BinaryTranslation] added some HardwareNode classes

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/hardware/tree/nodes/declaration/WireDeclaration.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/hardware/tree/nodes/declaration/WireDeclaration.java
@@ -2,6 +2,19 @@ package pt.up.fe.specs.binarytranslation.hardware.tree.nodes.declaration;
 
 import pt.up.fe.specs.binarytranslation.hardware.tree.nodes.AHardwareNode;
 
-public class WireDeclaration extends AHardwareNode implements HardwareDeclaration {
+public class WireDeclaration extends AHardwareNode implements VariableDeclaration {
 
+    private final String wireName;
+    private final int numBits;
+
+    public WireDeclaration(String wireName, int numBits) {
+        super();
+        this.wireName = wireName;
+        this.numBits = numBits;
+    }
+
+    @Override
+    public String getAsString() {
+        return "wire [ (" + this.numBits + " - 1) : 0] " + this.wireName + ";\n";
+    }
 }
