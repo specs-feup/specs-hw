f9239b1baff903056e3ffb32830226126268e0bb
nmcp88 <nmcp88@gmail.com>
2020-08-03 17:09:36 +0100

    [RISCV] missing tester file

---
+++ RISCV/test/org/specs/Riscv/test/RiscvElfStreamTester.java
@@ -0,0 +1,25 @@
+package org.specs.Riscv.test;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.specs.Riscv.stream.RiscvElfStream;
+
+import pt.up.fe.specs.util.SpecsIo;
+
+public class RiscvElfStreamTester {
+
+    @Test
+    public void test() {
+        File fd = SpecsIo.resourceCopy("org/specs/Riscv/asm/test64.elf");
+        fd.deleteOnExit();
+
+        try (RiscvElfStream el = new RiscvElfStream(fd)) {
+            el.rawDump();
+            /*Instruction inst = null;
+            while ((inst = el.nextInstruction()) != null) {
+                inst.printInstruction();
+            }*/
+        }
+    }
+}
