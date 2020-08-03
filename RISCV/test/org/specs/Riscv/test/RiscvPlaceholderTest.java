940e6513503c4d06e26a686ce4902101cb215a3d
nmcp88 <nmcp88@gmail.com>
2020-08-03 12:51:56 +0100

    [RISCV] added placeholder test

---
+++ RISCV/test/org/specs/Riscv/test/RiscvPlaceholderTest.java
@@ -0,0 +1,12 @@
+package org.specs.Riscv.test;
+
+import org.junit.jupiter.api.Test;
+
+class RiscvPlaceholderTest {
+
+    @Test
+    void test() {
+        // fail("Not yet implemented");
+    }
+
+}
