7d2a268a1e66f334dcffb63f36c87b24b40ab7bc
nmcp88 <nmcp88@gmail.com>
2020-08-03 17:08:21 +0100

    [ARM] fixed some fields in ArmResource, other minor refactoring (no functional changes)

--- ARM/src/org/specs/Arm/ArmResource.java
+++ ARM/src/org/specs/Arm/ArmResource.java
@@ -5,9 +5,14 @@ import pt.up.fe.specs.util.providers.ResourceProvider;
 public enum ArmResource implements ResourceProvider {
 
     ARMv8_CPU_NAME("aarch64"),
+
     QEMU_AARCH64_GDB_TEMPLATE("org/specs/Arm/gdb/qemutmpl.gdb"),
     QEMU_AARCH64_BAREMETAL_DTB("org/specs/Arm/qemu/zcu102-arm.dtb"),
+    QEMU_AARCH64_EXE("qemu-system-aarch64"),
+    // https://www.xilinx.com/video/soc/running-bare-metal-application-using-qemu-command-line.html
+
     AARCH64_GCC("aarch64-none-elf-gcc"),
+    AARCH64_GDB("aarch64-none-elf-gdb"),
     AARCH64_READELF("aarch64-none-elf-readelf");
 
     private final String resource;
