103336873bdb316aa50bbc5e741633292851616f
nmcp88 <nmcp88@gmail.com>
2020-08-04 12:29:20 +0100

    [RISCV] added more classes to complete the instruction/operand family for this ISA

--- RISCV/src/org/specs/Riscv/parsing/RiscvAsmFieldData.java
+++ RISCV/src/org/specs/Riscv/parsing/RiscvAsmFieldData.java
@@ -1,5 +1,9 @@
 package org.specs.Riscv.parsing;
 
+import static org.specs.Riscv.instruction.RiscvOperand.*;
+import static org.specs.Riscv.parsing.RiscvAsmField.*;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,7 +48,80 @@ public class RiscvAsmFieldData extends AsmFieldData {
      * in the AsmFields
      */
     public List<Operand> getOperands() {
-        return null; // TODO
-    }
 
+        RiscvAsmFieldType type = (RiscvAsmFieldType) this.get(TYPE);
+        var map1 = this.get(FIELDS);
+
+        // get int values from fields
+        Map<RiscvAsmField, Integer> operandmap = new HashMap<RiscvAsmField, Integer>();
+        for (RiscvAsmField field : RiscvAsmField.values()) {
+            if (map1.containsKey(field.getFieldName())) {
+                operandmap.put(field, Integer.parseInt(map1.get(field.getFieldName()), 2));
+            }
+        }
+
+        int fullimm = 0;
+
+        // assign to Operand objects based on field format
+        List<Operand> operands = new ArrayList<Operand>();
+
+        // order of operands MUST be preserved (or should be)
+        switch (type) {
+
+        ///////////////////////////////////////////////////////////////////////
+        case R:
+            operands.add(newWriteRegister(RD, operandmap.get(RD)));
+            operands.add(newReadRegister(RS1, operandmap.get(RS1)));
+            operands.add(newReadRegister(RS2, operandmap.get(RS2)));
+            break;
+
+        ///////////////////////////////////////////////////////////////////////
+        case I:
+        case LOAD:
+        case JALR:
+            operands.add(newWriteRegister(RD, operandmap.get(RD)));
+            operands.add(newReadRegister(RS1, operandmap.get(RS1)));
+            operands.add(newImmediate(IMMTWELVE, operandmap.get(IMMTWELVE)));
+            break;
+
+        ///////////////////////////////////////////////////////////////////////
+        case S:
+            operands.add(newWriteRegister(RD, operandmap.get(RD)));
+            operands.add(newReadRegister(RS1, operandmap.get(RS1)));
+
+            // build full imm field from 2 fields
+            var imm7 = Integer.valueOf(operandmap.get(IMMSEVEN));
+            var imm5 = Integer.valueOf(operandmap.get(IMMFIVE));
+            fullimm = (imm7 << 5) | imm5;
+            operands.add(newImmediate(IMM, fullimm));
+            break;
+
+        ///////////////////////////////////////////////////////////////////////
+        case U:
+        case UJ:
+            operands.add(newWriteRegister(RD, operandmap.get(RD)));
+            operands.add(newImmediate(IMMTWENTY, operandmap.get(IMMTWENTY)));
+            break;
+
+        ///////////////////////////////////////////////////////////////////////
+        case SB:
+            operands.add(newReadRegister(RS1, operandmap.get(RS1)));
+            operands.add(newReadRegister(RS2, operandmap.get(RS2)));
+
+            // build full imm field from 4 fields
+            var bit12 = Integer.valueOf(operandmap.get(BIT12));
+            var bit11 = Integer.valueOf(operandmap.get(BIT11));
+            var imm6 = Integer.valueOf(operandmap.get(IMMSIX));
+            var imm4 = Integer.valueOf(operandmap.get(IMMFOUR));
+            fullimm = (bit12 << 12) | (bit11 << 11) | (imm6 << 5) | (imm4 << 1);
+            operands.add(newImmediate(IMM, fullimm));
+            break;
+
+        ///////////////////////////////////////////////////////////////////////
+        default:
+            break;
+        }
+
+        return operands;
+    }
 }
--- RISCV/src/org/specs/Riscv/parsing/RiscvInstructionParsers.java
+++ RISCV/src/org/specs/Riscv/parsing/RiscvInstructionParsers.java
@@ -26,20 +26,32 @@ public interface RiscvInstructionParsers {
     // see "https://www2.eecs.berkeley.edu/Pubs/TechRpts/2014/EECS-2014-54.pdf"
     // page 49 for encodings for field "opcode"
     List<AsmParser> PARSERS = Arrays.asList(
-            newInstance(R, "funct7(7)_rs2(5)_rs1(5)_funct3(3)_rd(5)_0110000(7)"), // OP encoding for opcode
-            newInstance(I, "imm12bits(12)_rs1(5)_funct3(3)_rd(5)_0010000(7)"), // OP-IMM encoding for opcode
-            newInstance(S, "imm7bits(7)_rs2(5)_rs1(5)_funct3(3)_imm5bits(5)_0100100(7)"), // STORE encoding for opcode
-            newInstance(U, "imm20bits(20)_rd(5)_0_opcode_101"), // U-type, used for AUIPC (opcode=0) and LUI (opcode=1)
-            newInstance(LOAD, "imm12bits(1)_rs1(5)_funct3(3)_rd(5)_0000000"), // LOAD encoding for opcode (other fields
-                                                                              // equal to
-            // I-Type)
 
-            newInstance(JALR, "imm12bits(12)_rs1(5)_funct3(3)_rd(5)_1100100"), // JALR encoding for opcode (other fields
-                                                                               // equal to
-            // I-Type)
+            newInstance(R, "funct7(7)_rs2(5)_rs1(5)_funct3(3)_rd(5)_0110011"), // OP encoding for opcode
 
-            newInstance(SB, "bit12_imm6bits(6)_rs2(5)_rs1(5)_funct3(3)_imm4bits(4)_bit11_1100000"), // BRANCH encoding
-                                                                                                    // for opcode
-            newInstance(UJ, "bit20_imm10bits(10)_bit11_imm8bits(8)_rd(5)_1101100") // unconditional jumps (JAL encoding)
-    );
+            newInstance(I, "immtwelve(12)_rs1(5)_funct3(3)_rd(5)_0010011"), // OP-IMM encoding for opcode
+
+            newInstance(S, "immseven(7)_rs2(5)_rs1(5)_funct3(3)_immfive(5)_0100011"), // STORE encoding for
+                                                                                      // opcode
+
+            newInstance(U, "immtwenty(20)_rd(5)_0_opcode(1)_10111"), // U-type, used for AUIPC (opcode=0) and LUI
+            // (opcode=1)
+
+            newInstance(LOAD, "immtwelve(1)_rs1(5)_funct3(3)_rd(5)_0000011"), // LOAD encoding for opcode (other
+                                                                              // fields
+            // equal to I-Type)
+
+            newInstance(JALR, "immtwelve(12)_rs1(5)_funct3(3)_rd(5)_1100111"), // JALR encoding for opcode (other
+                                                                               // fields
+            // equal to I-Type)
+
+            newInstance(SB, "bit12(1)_immsix(6)_rs2(5)_rs1(5)_funct3(3)_immfour(4)_bit11(1)_1100011"), // BRANCH
+            // encoding
+            // for opcode
+
+            newInstance(UJ, "bit20(1)_immten(10)_bit11(1)_immeight(8)_rd(5)_1101111"), // unconditional jumps
+                                                                                       // (JAL
+            // encoding)
+
+            newInstance(UNDEFINED, "x(32)"));
 }
