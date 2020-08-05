20e51dda67e9c29a71f9db8b7c58f36b54262dc7
nmcp88 <nmcp88@gmail.com>
2020-08-05 13:35:42 +0100

    [BinaryTranslation] added new abstracted tester utils
    Utils can be reused for all ISAs (e.g. frequent sequence detection test
    for MicroBlaze or ARM with a single line call)

---
+++ BinaryTranslation/test/pt/up/fe/specs/binarytranslation/test/instruction/SymbolifyTestUtils.java
@@ -0,0 +1,40 @@
+package pt.up.fe.specs.binarytranslation.test.instruction;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import pt.up.fe.specs.binarytranslation.binarysegments.detection.BinarySegmentDetectionUtils;
+import pt.up.fe.specs.binarytranslation.binarysegments.detection.HashedSequence;
+import pt.up.fe.specs.binarytranslation.instruction.Instruction;
+
+public class SymbolifyTestUtils {
+
+    public static void symbolifyTest(Instruction i) {
+
+        Method method = null;
+        try {
+            method = BinarySegmentDetectionUtils.class.getDeclaredMethod("hashSequence", List.class);
+            method.setAccessible(true);
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        // hash instruction
+        var l = Arrays.asList(i);
+        HashedSequence hs = null;
+        try {
+            hs = (HashedSequence) method.invoke(BinarySegmentDetectionUtils.class, l);
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        // TEST THIS:
+        hs.makeSymbolic();
+    }
+}
