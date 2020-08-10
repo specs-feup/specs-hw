d6cb7c9a9baea545be44f77c3ca64699ed3b92f1
nmcp88 <nmcp88@gmail.com>
2020-08-10 09:11:04 +0100

    [BinaryTranslation] missing files for HardwareNode family

---
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/hardware/tree/nodes/statement/IfStatement.java
@@ -0,0 +1,51 @@
+package pt.up.fe.specs.binarytranslation.hardware.tree.nodes.statement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.up.fe.specs.binarytranslation.hardware.tree.nodes.expression.HardwareExpression;
+
+public class IfStatement extends HardwareStatement {
+
+    public IfStatement(HardwareExpression expr) {
+        this.addChild(expr);
+    }
+
+    public void addStatement(HardwareStatement stat) {
+        this.addChild(stat);
+    }
+
+    public HardwareExpression getCondition() {
+        return (HardwareExpression) this.getChild(0);
+    }
+
+    public List<HardwareStatement> getStatements() {
+        var list = this.getChildren();
+        var nlist = new ArrayList<HardwareStatement>();
+        for (var c : list)
+            nlist.add((HardwareStatement) c);
+        return nlist;
+    }
+
+    @Override
+    public String getAsString() {
+        var builder = new StringBuilder();
+
+        builder.append("if(" + this.getCondition().getAsString() + ")");
+
+        var stats = this.getStatements();
+        if (stats.size() > 1)
+            builder.append(" begin");
+        builder.append("\n\t");
+
+        for (var child : stats) {
+            builder.append("\t" + child.getAsString() + "\n");
+        }
+
+        if (this.getStatements().size() > 1)
+            builder.append("end");
+        builder.append("\n");
+
+        return builder.toString();
+    }
+}
