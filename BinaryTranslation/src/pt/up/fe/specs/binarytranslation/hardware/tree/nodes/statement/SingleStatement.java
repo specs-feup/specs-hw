cf962f95156c323340f19a2dd30177a6d6a258c0
nmcp88 <nmcp88@gmail.com>
2020-08-10 09:11:04 +0100

    [BinaryTranslation] missing files for HardwareNode family

---
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/hardware/tree/nodes/statement/SingleStatement.java
@@ -0,0 +1,21 @@
+package pt.up.fe.specs.binarytranslation.hardware.tree.nodes.statement;
+
+import pt.up.fe.specs.binarytranslation.hardware.tree.nodes.expression.HardwareExpression;
+import pt.up.fe.specs.binarytranslation.hardware.tree.nodes.expression.VariableReference;
+
+public class SingleStatement extends HardwareStatement {
+
+    protected SingleStatement(VariableReference target, HardwareExpression expression) {
+        super();
+        this.addChild(target);
+        this.addChild(expression);
+    }
+
+    protected VariableReference getTarget() {
+        return (VariableReference) this.getChild(0);
+    }
+
+    protected HardwareExpression getExpression() {
+        return (HardwareExpression) this.getChild(1);
+    }
+}
