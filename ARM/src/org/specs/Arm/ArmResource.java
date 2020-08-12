028bfb7702bda17c326ba9ddd7ffb87deffa8d79
nmcp88 <nmcp88@gmail.com>
2020-08-12 16:12:00 +0100

    [ARM] Added some tester code; added ivy.xml

--- ARM/src/org/specs/Arm/ArmResource.java
+++ ARM/src/org/specs/Arm/ArmResource.java
@@ -5,18 +5,21 @@ import pt.up.fe.specs.util.providers.ResourceProvider;
 public enum ArmResource implements ResourceProvider {
 
     // README:
-    // How to QEMU for AARCH64 after running "sudo apt-get upgrade qemu*"
-    // 1. Source the settings64.sh script for Vivado
-    // 2. do "unset LD_LIBRARY_PATH" so that the /usr/bin/qemu-system-aarch64 executable doesn't try to use the
-    // libusb libs that come packaged with Vivado (they are older libs without libusbredirparser)
-    // 3. use the "qemutmpl_nodtb.gbd" script, since the new version of qemu-system-aarch64 apparently has
-    // issues with the "arm-generic-fdt" machine target after upgrade combined with sourcing of vivado script...
+    // One thing to remember is that the libusb libs that come with Vivado are probably meant to be used with
+    // the QEMU versions found in https://github.com/Xilinx/qemu
+    // That is, if you checkout a new QEMU build, you may need to install the lastest vivado, or at least
+    // force QEMU to rely on system libs!
+    // Regardless, in order to make the trace stream for aarch64 work currently:
+    // 1. Source Vivado settings script
+    // 2. run "unset LD_LIBRARY_PATH"
+    // 3. Use the "qemutmpl.dtb" template, which relies on the arm-generic-fdt ONLY SUPPORTED BY THE XILINX BUILD OF
+    // QEMU!
 
     ARMv8_CPU_NAME("aarch64"),
 
-    QEMU_AARCH64_GDB_TEMPLATE("org/specs/Arm/gdb/qemutmpl_nodtb.gdb"),
+    QEMU_AARCH64_GDB_TEMPLATE("org/specs/Arm/gdb/qemutmpl.gdb"),
     QEMU_AARCH64_BAREMETAL_DTB("org/specs/Arm/qemu/zcu102-arm.dtb"),
-    QEMU_AARCH64_EXE("qemu-system-aarch64"),
+    QEMU_AARCH64_EXE("/media/nuno/HDD/work/projects/xilinx-repos/qemu/aarch64-softmmu/qemu-system-aarch64"),
     // https://www.xilinx.com/video/soc/running-bare-metal-application-using-qemu-command-line.html
 
     AARCH64_GCC("aarch64-none-elf-gcc"),
