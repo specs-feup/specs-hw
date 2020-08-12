fbc11e3f40d7c9c02b956edd1b43229e4e3ddc73
nmcp88 <nmcp88@gmail.com>
2020-08-12 11:31:48 +0100

    [ARM] fixed QEMU execution after upgrading QEMU

--- ARM/resources/org/specs/Arm/gdb/qemutmpl_nodtb.gdb
+++ ARM/resources/org/specs/Arm/gdb/qemutmpl_nodtb.gdb
@@ -4,6 +4,7 @@ set print address off
 set height 0
 file <ELFNAME>
 target remote | <QEMUBIN> -M virt -cpu cortex-a53 -nographic -kernel <ELFNAME> -chardev stdio,mux=on,id=char0 -mon chardev=char0,mode=readline -serial chardev:char0 -gdb chardev:char0 -S
+stepi 1
 set $v = 0
 while $pc != $v
 set $v = $pc
--- ARM/src/org/specs/Arm/ArmResource.java
+++ ARM/src/org/specs/Arm/ArmResource.java
@@ -4,9 +4,17 @@ import pt.up.fe.specs.util.providers.ResourceProvider;
 
 public enum ArmResource implements ResourceProvider {
 
+    // README:
+    // How to QEMU for AARCH64 after running "sudo apt-get upgrade qemu*"
+    // 1. Source the settings64.sh script for Vivado
+    // 2. do "unset LD_LIBRARY_PATH" so that the /usr/bin/qemu-system-aarch64 executable doesn't try to use the
+    // libusb libs that come packaged with Vivado (they are older libs without libusbredirparser)
+    // 3. use the "qemutmpl_nodtb.gbd" script, since the new version of qemu-system-aarch64 apparently has
+    // issues with the "arm-generic-fdt" machine target after upgrade combined with sourcing of vivado script...
+
     ARMv8_CPU_NAME("aarch64"),
 
-    QEMU_AARCH64_GDB_TEMPLATE("org/specs/Arm/gdb/qemutmpl.gdb"),
+    QEMU_AARCH64_GDB_TEMPLATE("org/specs/Arm/gdb/qemutmpl_nodtb.gdb"),
     QEMU_AARCH64_BAREMETAL_DTB("org/specs/Arm/qemu/zcu102-arm.dtb"),
     QEMU_AARCH64_EXE("qemu-system-aarch64"),
     // https://www.xilinx.com/video/soc/running-bare-metal-application-using-qemu-command-line.html
