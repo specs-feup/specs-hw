629ed41da978d5883e18b8cf1015b477dae836eb
nmcp88 <nmcp88@gmail.com>
2020-09-07 11:11:50 +0100

    [BinaryTranslation] adding some classes for instruction stream profiling

---
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/binarysegments/detection/SegmentDetectorBuilder.java
@@ -0,0 +1,40 @@
+package pt.up.fe.specs.binarytranslation.binarysegments.detection;
+
+import pt.up.fe.specs.binarytranslation.binarysegments.BinarySegmentType;
+import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
+
+public class SegmentDetectorBuilder {
+
+    /*
+     * 
+     */
+    public static SegmentDetector buildDetector(InstructionStream istream, BinarySegmentType segtype) {
+
+        SegmentDetector detector = null;
+        switch (segtype) {
+        case STATIC_FREQUENT_SEQUENCE:
+            detector = new FrequentStaticSequenceDetector(istream);
+            break;
+
+        case TRACE_FREQUENT_SEQUENCE:
+            detector = new FrequentTraceSequenceDetector(istream);
+            break;
+
+        case STATIC_BASIC_BLOCK:
+            detector = new StaticBasicBlockDetector(istream);
+            break;
+
+        case TRACE_BASIC_BLOCK:
+            detector = new TraceBasicBlockDetector(istream);
+            break;
+
+        case MEGA_BLOCK:
+            break;
+
+        default:
+            break;
+        }
+
+        return detector;
+    }
+}
