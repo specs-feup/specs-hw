b188720e7e418522f0ceb2f0a23a2438c4c7d0d0
nmcp88 <nmcp88@gmail.com>
2020-09-10 08:55:46 +0100

    [RISCV] Refactored some class names and package names

--- RISCV/src/org/specs/Riscv/stream/RiscvElfStream.java
+++ RISCV/src/org/specs/Riscv/stream/RiscvElfStream.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 import org.specs.Riscv.RiscvResource;
 
-import pt.up.fe.specs.binarytranslation.BinaryTranslationUtils;
-import pt.up.fe.specs.binarytranslation.asm.ApplicationInformation;
+import pt.up.fe.specs.binarytranslation.asm.Application;
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.stream.AStaticInstructionStream;
+import pt.up.fe.specs.binarytranslation.utils.BinaryTranslationUtils;
 import pt.up.fe.specs.util.utilities.LineStream;
 
 public class RiscvElfStream extends AStaticInstructionStream {
@@ -17,7 +17,7 @@ public class RiscvElfStream extends AStaticInstructionStream {
 
     public RiscvElfStream(File elfname) {
         super(elfname, RiscvResource.RISCV_OBJDUMP.getResource());
-        this.appInfo = new ApplicationInformation(
+        this.appInfo = new Application(
                 RiscvResource.RISCV_CPU_NAME.getResource(), elfname.getName(),
                 BinaryTranslationUtils.getCompilationInfo(elfname.getPath(),
                         RiscvResource.RISCV_READELF.getResource()));
--- RISCV/src/org/specs/Riscv/stream/RiscvTraceStream.java
+++ RISCV/src/org/specs/Riscv/stream/RiscvTraceStream.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 import org.specs.Riscv.RiscvResource;
 
-import pt.up.fe.specs.binarytranslation.BinaryTranslationUtils;
-import pt.up.fe.specs.binarytranslation.asm.ApplicationInformation;
+import pt.up.fe.specs.binarytranslation.asm.Application;
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.stream.ATraceInstructionStream;
+import pt.up.fe.specs.binarytranslation.utils.BinaryTranslationUtils;
 
 public class RiscvTraceStream extends ATraceInstructionStream {
 
@@ -20,7 +20,7 @@ public class RiscvTraceStream extends ATraceInstructionStream {
                 RiscvResource.QEMU_RISCV_BAREMETAL_DTB,
                 RiscvResource.QEMU_RISCV_EXE);
 
-        this.appInfo = new ApplicationInformation(
+        this.appInfo = new Application(
                 RiscvResource.RISCV_CPU_NAME.getResource(), elfname.getName(),
                 BinaryTranslationUtils.getCompilationInfo(elfname.getPath(),
                         RiscvResource.RISCV_READELF.getResource()));
