620fbe007700b264475139564d25191ef0976e89
nmcp88 <nmcp88@gmail.com>
2020-10-12 14:47:19 +0100

    [BinaryTranslation] pre-working version of multistream
    with threaded producer-consumers

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/profiling/InstructionStreamProfiler.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/profiling/InstructionStreamProfiler.java
@@ -1,11 +1,9 @@
 package pt.up.fe.specs.binarytranslation.profiling;
 
-import java.util.concurrent.Callable;
-
 import pt.up.fe.specs.binarytranslation.profiling.data.InstructionProfileResult;
 
-public interface InstructionStreamProfiler extends Callable<InstructionProfileResult> { // extends
-                                                                                        // InstructionStreamConsumer {
+public interface InstructionStreamProfiler { // extends
+                                             // InstructionStreamConsumer {
 
     /*
      * Run the profiling over the stream
