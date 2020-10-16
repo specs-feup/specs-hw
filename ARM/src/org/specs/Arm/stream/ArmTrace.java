2ea09111cbcc44c2bd983afb957dfb4cfc856cbd
nmcp88 <nmcp88@gmail.com>
2020-10-16 15:28:12 +0100

    [ARM] new ArmApplication and Arm Provider children classes

---
+++ ARM/src/org/specs/Arm/stream/ArmTrace.java
@@ -0,0 +1,18 @@
+package org.specs.Arm.stream;
+
+import java.io.File;
+
+import org.specs.Arm.ArmApplication;
+import org.specs.Arm.ArmResource;
+import org.specs.Arm.instruction.ArmInstruction;
+
+import pt.up.fe.specs.binarytranslation.producer.TraceInstructionProducer;
+
+public class ArmTrace extends TraceInstructionProducer {
+
+    public ArmTrace(File elfname) {
+        super(new ArmApplication(elfname),
+                ArmResource.AARCH64_REGEX,
+                ArmInstruction::newInstance);
+    }
+}
