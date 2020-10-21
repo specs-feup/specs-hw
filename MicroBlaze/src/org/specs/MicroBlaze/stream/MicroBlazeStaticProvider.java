ac7ce485b21c4308b263a784e5a022430d68cd1d
nmcp88 <nmcp88@gmail.com>
2020-10-21 16:50:29 +0100

    [MicroBlaze] minor class re-namings and method abstractions; test fixes

--- MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeStaticProvider.java
+++ MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeStaticProvider.java
@@ -2,15 +2,15 @@ package org.specs.MicroBlaze.stream;
 
 import java.io.File;
 
-import org.specs.MicroBlaze.MicroBlazeApplication;
 import org.specs.MicroBlaze.MicroBlazeResource;
+import org.specs.MicroBlaze.asm.MicroBlazeApplication;
 import org.specs.MicroBlaze.instruction.MicroBlazeInstruction;
 
 import pt.up.fe.specs.binarytranslation.producer.StaticInstructionProducer;
 
-public class MicroBlazeDump extends StaticInstructionProducer {
+public class MicroBlazeStaticProvider extends StaticInstructionProducer {
 
-    public MicroBlazeDump(File elfname) {
+    public MicroBlazeStaticProvider(File elfname) {
         super(new MicroBlazeApplication(elfname),
                 MicroBlazeResource.MICROBLAZE_INSTRUCTION_DUMP_REGEX,
                 MicroBlazeInstruction::newInstance);
--- MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceProvider.java
+++ MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceProvider.java
@@ -2,15 +2,15 @@ package org.specs.MicroBlaze.stream;
 
 import java.io.File;
 
-import org.specs.MicroBlaze.MicroBlazeApplication;
 import org.specs.MicroBlaze.MicroBlazeResource;
+import org.specs.MicroBlaze.asm.MicroBlazeApplication;
 import org.specs.MicroBlaze.instruction.MicroBlazeInstruction;
 
 import pt.up.fe.specs.binarytranslation.producer.TraceInstructionProducer;
 
-public class MicroBlazeTrace extends TraceInstructionProducer {
+public class MicroBlazeTraceProvider extends TraceInstructionProducer {
 
-    public MicroBlazeTrace(File elfname) {
+    public MicroBlazeTraceProvider(File elfname) {
         super(new MicroBlazeApplication(elfname),
                 MicroBlazeResource.MICROBLAZE_INSTRUCTION_TRACE_REGEX,
                 MicroBlazeInstruction::newInstance);
