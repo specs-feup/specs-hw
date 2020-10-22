621c064f662bb151f46c8b9f7a136b16f040d2a2
nmcp88 <nmcp88@gmail.com>
2020-10-22 21:46:28 +0100

    [BinaryTranslation] new abstract ASimpleSegmentDetector
    abstracted away the contents of AFixedSizeBasicBlockDetector and
    AFixedSizeFrequentSequenceDetector into a parent class; only the
    validity rule is different for both cases

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/detection/detectors/fixed/AFixedSizeBasicBlockDetector.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/detection/detectors/fixed/AFixedSizeBasicBlockDetector.java
@@ -1,15 +1,7 @@
 package pt.up.fe.specs.binarytranslation.detection.detectors.fixed;
 
-import java.util.List;
-import java.util.Map;
-
-import pt.up.fe.specs.binarytranslation.detection.detectors.ASegmentDetector;
-import pt.up.fe.specs.binarytranslation.detection.detectors.BinarySegmentDetectionUtils;
 import pt.up.fe.specs.binarytranslation.detection.detectors.DetectorConfiguration;
-import pt.up.fe.specs.binarytranslation.detection.detectors.HashedSequence;
 import pt.up.fe.specs.binarytranslation.detection.trace.InstructionWindow;
-import pt.up.fe.specs.binarytranslation.instruction.Instruction;
-import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
 
 /**
  * Basic block detector which only detects blocks of a specific size
@@ -17,7 +9,7 @@ import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
  * @author nuno
  *
  */
-public abstract class AFixedSizeBasicBlockDetector extends ASegmentDetector {
+public abstract class AFixedSizeBasicBlockDetector extends ASimpleSegmentDetector {
 
     /*
      * Constructor
@@ -27,54 +19,26 @@ public abstract class AFixedSizeBasicBlockDetector extends ASegmentDetector {
     }
 
     /*
-     * 
+     * Check if candidate sequence is valid
      */
     @Override
-    public void processStream(InstructionStream istream, Map<String, HashedSequence> hashed,
-            Map<Integer, List<Integer>> addrs) {
+    protected Boolean validSequence(InstructionWindow window) {
 
-        var window = new InstructionWindow(this.getConfig().getMaxsize());
+        // check precendent requirements first!
+        if (super.validSequence(window) == false)
+            return false;
 
-        // make 1st window
-        while (!window.isFull())
-            window.add(istream.nextInstruction());
+        // must obey these conditions
+        var last = window.getLast();
+        if (!(last.isBackwardsJump() && last.isConditionalJump() && last.isRelativeJump()))
+            return false;
 
-        // process entire stream
-        Instruction is = null;
-        while ((is = istream.nextInstruction()) != null) {
+        // target isn't start of window, skip this candidate
+        int targetAddr = last.getBranchTarget().intValue(); // TODO what if branch is based on register values?
+        int firstAddr = window.get(0).getAddress().intValue();
+        if (targetAddr != firstAddr)
+            return false;
 
-            // insert one
-            window.add(is);
-
-            // "is" is a possible backwards branch that could generate a basic block, so, try to hash it
-            if (is.isBackwardsJump() && is.isConditionalJump() && is.isRelativeJump()) {
-
-                // adjust to delay of last instruction in window
-                var delay = window.getLast().getDelay();
-                while (delay-- > 0)
-                    window.add(istream.nextInstruction());
-
-                // compute target of backwards branch
-                int targetAddr = is.getBranchTarget().intValue();
-                int firstAddr = window.get(0).getAddress().intValue();
-
-                // target isn't start of window, skip this candidate
-                if (targetAddr != firstAddr)
-                    continue;
-
-                // else, create new candidate hash sequence
-                var newseq = BinarySegmentDetectionUtils.hashSequence(window.getWindow());
-
-                // add sequence to occurrence counters
-                // (counting varies between static to trace detection)
-                BinarySegmentDetectionUtils.addAddrToList(addrs, newseq);
-
-                // add sequence to map which is indexed by hashCode + startaddr
-                BinarySegmentDetectionUtils.addHashSequenceToList(hashed, newseq);
-            }
-        }
-
-        // Remove all sequences which only happen once
-        BinarySegmentDetectionUtils.removeUnique(addrs, hashed);
+        return true;
     }
 }
