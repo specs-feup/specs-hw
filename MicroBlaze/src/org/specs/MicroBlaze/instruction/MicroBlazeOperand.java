b096f87351a4b5d556d411f1c253146d699c79af
nmcp88 <nmcp88@gmail.com>
2020-10-23 15:53:12 +0100

    [MicroBlaze] refactoring MicroBlazeOperand instantiation methods

--- MicroBlaze/src/org/specs/MicroBlaze/instruction/MicroBlazeOperand.java
+++ MicroBlaze/src/org/specs/MicroBlaze/instruction/MicroBlazeOperand.java
@@ -1,34 +1,39 @@
 package org.specs.MicroBlaze.instruction;
 
-import static pt.up.fe.specs.binarytranslation.instruction.operand.OperandType.*;
-
 import org.specs.MicroBlaze.parsing.MicroBlazeAsmField;
 
-import pt.up.fe.specs.binarytranslation.instruction.operand.*;
+import pt.up.fe.specs.binarytranslation.instruction.operand.AOperand;
+import pt.up.fe.specs.binarytranslation.instruction.operand.AOperandProperties;
+import pt.up.fe.specs.binarytranslation.instruction.operand.OperandAccessType;
+import pt.up.fe.specs.binarytranslation.instruction.operand.OperandDataSize;
+import pt.up.fe.specs.binarytranslation.instruction.operand.OperandDataType;
+import pt.up.fe.specs.binarytranslation.instruction.operand.OperandProperties;
+import pt.up.fe.specs.binarytranslation.instruction.operand.OperandType;
 
 public class MicroBlazeOperand extends AOperand {
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -1023924152110388490L;
 
     private MicroBlazeOperand(OperandProperties props, int value) {
         super(props, value);
     }
 
     public static MicroBlazeOperand newReadRegister(MicroBlazeAsmField field, int value) {
-        var props = new AOperandProperties(field, "r", "", 32, REGISTER, READ, WORD);
+        var props = new AOperandProperties(field, "r", "",
+                OperandType.REGISTER, OperandAccessType.READ,
+                OperandDataType.SCALAR_INTEGER, OperandDataSize.WORD);
         return new MicroBlazeOperand(props, value);
     }
 
     public static MicroBlazeOperand newWriteRegister(MicroBlazeAsmField field, int value) {
-        var props = new AOperandProperties(field, "r", "", 32, REGISTER, WRITE, WORD);
+        var props = new AOperandProperties(field, "r", "",
+                OperandType.REGISTER, OperandAccessType.WRITE,
+                OperandDataType.SCALAR_INTEGER, OperandDataSize.WORD);
         return new MicroBlazeOperand(props, value);
     }
 
     public static MicroBlazeOperand newImmediate(MicroBlazeAsmField field, int value) {
-        var props = new AOperandProperties(field, "0x", "", 32, IMMEDIATE, READ, WORD);
+        var props = new AOperandProperties(field, "0x", "",
+                OperandType.IMMEDIATE, OperandAccessType.READ,
+                OperandDataType.SCALAR_INTEGER, OperandDataSize.WORD);
         return new MicroBlazeOperand(props, value);
     }
 
