ea5ee302eb49528c36c7dd2279bfcdbc31f781e8
nmcp88 <nmcp88@gmail.com>
2021-01-10 20:55:33 +0000

    [ARM] changed hardcoded elf paths into ELFProviders

--- ARM/test/org/specs/Arm/asm/detection/ArmStaticBasicBlockDetectorTester.java
+++ ARM/test/org/specs/Arm/asm/detection/ArmStaticBasicBlockDetectorTester.java
@@ -1,6 +1,7 @@
 package org.specs.Arm.asm.detection;
 
 import org.junit.Test;
+import org.specs.Arm.ArmLivermoreELFN10;
 import org.specs.Arm.stream.ArmElfStream;
 import org.specs.Arm.stream.ArmTraceStream;
 
@@ -12,14 +13,14 @@ public class ArmStaticBasicBlockDetectorTester {
 
     @Test
     public void testStatic() {
-        var bundle = SegmentDetectTestUtils.detect("org/specs/Arm/asm/cholesky.elf",
+        var bundle = SegmentDetectTestUtils.detect(ArmLivermoreELFN10.cholesky,
                 ArmElfStream.class, StaticBasicBlockDetector.class);
         SegmentDetectTestUtils.printBundle(bundle, segment -> segment.getSegmentLength() == 4);
     }
 
     @Test
     public void testTrace() {
-        var bundle = SegmentDetectTestUtils.detect("org/specs/Arm/asm/cholesky.elf",
+        var bundle = SegmentDetectTestUtils.detect(ArmLivermoreELFN10.cholesky,
                 ArmTraceStream.class, TraceBasicBlockDetector.class);
         SegmentDetectTestUtils.printBundle(bundle);
     }
