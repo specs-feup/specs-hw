0da79e1ffebba2f7fa94b44f24928afb494083ac
nmcp88 <nmcp88@gmail.com>
2021-01-10 18:52:10 +0000

    [MicroBlaze] abstracted MicroBlaze specific classes from batch detec

--- MicroBlaze/test/org/specs/MicroBlaze/test/detection/MicroBlazeBatchFrequentSequenceTest.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/detection/MicroBlazeBatchFrequentSequenceTest.java
@@ -1,44 +1,64 @@
 package org.specs.MicroBlaze.test.detection;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import org.specs.BinaryTranslation.ELFProvider;
-import org.specs.MicroBlaze.MicroBlazeLivermoreELFN100;
+import org.specs.MicroBlaze.MicroBlazeLivermoreELFN10;
 import org.specs.MicroBlaze.stream.MicroBlazeElfStream;
-import org.specs.MicroBlaze.stream.MicroBlazeTraceProvider;
+import org.specs.MicroBlaze.stream.MicroBlazeStaticProvider;
 
 import pt.up.fe.specs.binarytranslation.detection.detectors.DetectorConfiguration.DetectorConfigurationBuilder;
 import pt.up.fe.specs.binarytranslation.detection.detectors.SegmentBundle;
-import pt.up.fe.specs.binarytranslation.detection.detectors.fixed.FrequentTraceSequenceDetector;
+import pt.up.fe.specs.binarytranslation.detection.detectors.fixed.FrequentStaticSequenceDetector;
+import pt.up.fe.specs.binarytranslation.graph.GraphBundle;
 import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
-import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.binarytranslation.utils.ClassBuilders;
+import pt.up.fe.specs.util.collections.concurrentchannel.ChannelConsumer;
 import pt.up.fe.specs.util.threadstream.ProducerEngine;
 
 public class MicroBlazeBatchFrequentSequenceTest {
 
-    private List<SegmentBundle> getSegments(ELFProvider elf) {
+    private List<SegmentBundle> getSegments(ELFProvider elf, int maxWindowSize,
+            Class<?> producerClass, Class<?> streamClass, Class<?> detectorClass) {
 
-        // file
-        File fd = SpecsIo.resourceCopy(elf.getResource());
-        fd.deleteOnExit();
+        var iproducer = ClassBuilders.buildProducer(producerClass, elf);
 
-        // producer
-        var iproducer = new MicroBlazeTraceProvider(fd);
+        /*
+         * Stream constructor
+         */
+        Constructor<?> cons;
+        try {
+            cons = streamClass.getConstructor(File.class, ChannelConsumer.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // host for threads
         var streamengine = new ProducerEngine<>(iproducer, op -> op.nextInstruction(),
-                cc -> new MicroBlazeElfStream(fd, cc));
+                cc -> {
+                    InstructionStream stream = null;
+                    try {
+                        stream = (InstructionStream) cons.newInstance(elf.getResource(), cc);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return stream;
+                });
 
         // for all window sizes
-        for (int i = 2; i < 10; i++) {
+        for (int i = 2; i < maxWindowSize; i++) {
 
-            var detector1 = new FrequentTraceSequenceDetector(
+            // detector
+            var detector = ClassBuilders.buildDetector(detectorClass,
                     new DetectorConfigurationBuilder().withMaxWindow(i).build());
-            streamengine.subscribe(istream -> detector1.detectSegments((InstructionStream) istream));
+
+            streamengine.subscribe(istream -> detector.detectSegments((InstructionStream) istream));
         }
 
         // launch all threads (blocking)
@@ -63,11 +83,16 @@ public class MicroBlazeBatchFrequentSequenceTest {
         profilerList.add(InstructionHistogram.class);*/
 
         // for (var file : MicroBlazeLivermoreELFN100.values()) {
-        for (var file : Arrays.asList(MicroBlazeLivermoreELFN100.innerprod100)) {
+        for (var elf : Arrays.asList(MicroBlazeLivermoreELFN10.innerprod)) {
 
-            var segs = getSegments(file);
+            var segs = getSegments(elf, 2,
+                    MicroBlazeStaticProvider.class,
+                    MicroBlazeElfStream.class,
+                    FrequentStaticSequenceDetector.class);
+
             for (var seg : segs) {
-                seg.toJSON();
+                var gbundle = GraphBundle.newInstance(seg);
+                gbundle.toJSON();
             }
 
             /*// for (var file : Arrays.asList(MicroBlazeLivermoreELFN100.linrec100)) {
