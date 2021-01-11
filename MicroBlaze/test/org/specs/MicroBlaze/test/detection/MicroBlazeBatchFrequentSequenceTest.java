93c6e8c9785152a91c1a85d0a031ab9e4ca74395
nmcp88 <nmcp88@gmail.com>
2021-01-11 22:13:43 +0000

    [MicroBlaze] changes to tester

--- MicroBlaze/test/org/specs/MicroBlaze/test/detection/MicroBlazeBatchFrequentSequenceTest.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/detection/MicroBlazeBatchFrequentSequenceTest.java
@@ -1,14 +1,13 @@
 package org.specs.MicroBlaze.test.detection;
 
 import org.junit.Test;
-import org.specs.MicroBlaze.MicroBlazeLivermoreELFN10;
+import org.specs.MicroBlaze.MicroBlazeLivermoreELFN100;
 import org.specs.MicroBlaze.stream.MicroBlazeElfStream;
 import org.specs.MicroBlaze.stream.MicroBlazeStaticProvider;
 
 import pt.up.fe.specs.binarytranslation.detection.detectors.fixed.FrequentStaticSequenceDetector;
 import pt.up.fe.specs.binarytranslation.graph.GraphBundle;
 import pt.up.fe.specs.binarytranslation.test.detection.ThreadedSegmentDetectUtils;
-import pt.up.fe.specs.binarytranslation.utils.BundleStatsUtils;
 
 public class MicroBlazeBatchFrequentSequenceTest {
 
@@ -18,9 +17,10 @@ public class MicroBlazeBatchFrequentSequenceTest {
     @Test
     public void MicroBlazeFrequentSequenceDetect() {
 
-        System.out.println("elf, window, maxcpl, avgcpl, maxilp, avgilp");
+        // System.out.println("elf, window, maxcpl, avgcpl, maxilp, avgilp, avgipc, maxipc");
+        System.out.println("elf, window, nrgraphs");
 
-        for (var elf : MicroBlazeLivermoreELFN10.values()) {
+        for (var elf : MicroBlazeLivermoreELFN100.values()) {
             // for (var elf : Arrays.asList(MicroBlazeLivermoreELFN10.innerprod)) {
 
             var segs = ThreadedSegmentDetectUtils.getSegments(elf, 20,
@@ -30,28 +30,48 @@ public class MicroBlazeBatchFrequentSequenceTest {
 
             for (var seg : segs) {
                 var gbundle = GraphBundle.newInstance(seg);
+                if (gbundle.getSegments().size() == 0) {
+                    System.out.println(seg.getApplicationInformation().getAppName()
+                            + ", " + 0 + ", " + 0);
+                    continue;
+                }
 
+                System.out.println(seg.getApplicationInformation().getAppName()
+                        + ", " + gbundle.getSegments().get(0).getSegmentLength()
+                        + ", " + gbundle.getSegments().size());
+
+                /*
                 if (gbundle.getSegments().size() == 0) {
                     System.out.println(seg.getApplicationInformation().getAppName()
                             + ", " + 0 + ", " + 0 + ", " + 0 + ", " + 0 + ", " + 0);
                     continue;
                 }
-
+                
+                gbundle.generateOutput();
+                
                 // cpl
                 var maxcpl = BundleStatsUtils.getMaxOf(
                         gbundle, func -> Integer.valueOf(func.getCpl()));
                 var avgcpl = BundleStatsUtils.getAverageOf(
                         gbundle, func -> Integer.valueOf(func.getCpl()));
-
+                
                 // ilp
                 var maxilp = BundleStatsUtils.getMaxOf(
                         gbundle, func -> Integer.valueOf(func.getMaxwidth()));
                 var avgilp = BundleStatsUtils.getAverageOf(
                         gbundle, func -> Integer.valueOf(func.getMaxwidth()));
-
+                
+                // ipc
+                var maxipc = BundleStatsUtils.getMaxOf(
+                        gbundle, func -> Integer.valueOf(Integer.valueOf((int) (func.getEstimatedIPC() * 10.0))));
+                var avgipc = BundleStatsUtils.getAverageOf(
+                        gbundle, func -> Integer.valueOf(Integer.valueOf((int) (func.getEstimatedIPC() * 10.0))));
+                
                 System.out.println(seg.getApplicationInformation().getAppName()
                         + ", " + gbundle.getSegments().get(0).getSegmentLength() + ", "
-                        + maxcpl + ", " + avgcpl + ", " + maxilp + ", " + avgilp);
+                        + maxcpl + ", " + avgcpl + ", " + maxilp + ", " + avgilp
+                        + ", " + (float) avgipc / 10.0f + ", " + (float) maxipc / 10.0f);
+                        */
             }
         }
     }
