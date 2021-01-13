c46398a9346214dacbbbb229b6f7697a3889480d
nmcp88 <nmcp88@gmail.com>
2021-01-13 17:00:53 +0000

    [RISCV] added testers

--- RISCV/test/org/specs/Riscv/test/detection/RiscvBatchFrequentSequenceTest.java
+++ RISCV/test/org/specs/Riscv/test/detection/RiscvBatchFrequentSequenceTest.java
@@ -1,32 +1,42 @@
 package org.specs.Riscv.test.detection;
 
 import org.junit.Test;
+import org.specs.BinaryTranslation.ELFProvider;
 import org.specs.Riscv.RiscvLivermoreELFN100iam;
 import org.specs.Riscv.stream.RiscvElfStream;
 import org.specs.Riscv.stream.RiscvStaticProvider;
+import org.specs.Riscv.stream.RiscvTraceProvider;
+import org.specs.Riscv.stream.RiscvTraceStream;
 
 import pt.up.fe.specs.binarytranslation.detection.detectors.fixed.FrequentStaticSequenceDetector;
+import pt.up.fe.specs.binarytranslation.detection.detectors.fixed.TraceBasicBlockDetector;
 import pt.up.fe.specs.binarytranslation.test.detection.ThreadedSegmentDetectUtils;
-import pt.up.fe.specs.binarytranslation.utils.BundleStatsUtils;
 
 public class RiscvBatchFrequentSequenceTest {
 
     /*
-     * Stats from frequent sequences
+     * Stats from static frequent sequences
      */
     @Test
     public void RiscvFrequentSequenceDetect() {
-
-        for (var elf : RiscvLivermoreELFN100iam.values()) {
-            // for (var elf : Arrays.asList(RiscvLivermoreELFN100iam.innerprod100)) {
-
-            var segs = ThreadedSegmentDetectUtils.getSegments(elf, 12,
-                    RiscvStaticProvider.class,
-                    RiscvElfStream.class,
-                    FrequentStaticSequenceDetector.class);
-
-            BundleStatsUtils.bundleStatsDump(elf, segs);
-        }
+        ELFProvider elfs[] = RiscvLivermoreELFN100iam.values();
+        // ELFProvider elfs[] = { RiscvLivermoreELFN100iam.innerprod100 };
+        ThreadedSegmentDetectUtils.BatchDetect(elfs, 2, 20,
+                RiscvStaticProvider.class,
+                RiscvElfStream.class,
+                FrequentStaticSequenceDetector.class);
     }
 
+    /*
+     * Stats from trace basic blocks
+     */
+    @Test
+    public void RiscvTraceBasicBlockDetect() {
+        ELFProvider elfs[] = RiscvLivermoreELFN100iam.values();
+        // ELFProvider elfs[] = { RiscvLivermoreELFN100iam.innerprod100 };
+        ThreadedSegmentDetectUtils.BatchDetect(elfs, 4, 50,
+                RiscvTraceProvider.class,
+                RiscvTraceStream.class,
+                TraceBasicBlockDetector.class);
+    }
 }
