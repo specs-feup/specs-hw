25a57b51c4f0744e3523feb6427689909345975e
tiagolascasas <tiagolascasas@outlook.com>
2021-02-24 21:37:21 +0000

    [RISCV] Improves analysis tests

--- RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
+++ RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
@@ -1,17 +1,13 @@
 package org.specs.Riscv.test.analysis;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 import org.junit.Test;
 import org.specs.Riscv.stream.detailed.RiscvDetailedTraceProvider;
 import org.specs.traceanalysis.TraceAnalysis;
 
-import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.producer.detailed.DetailedRegisterInstructionProducer;
-import pt.up.fe.specs.binarytranslation.producer.detailed.RegisterDump;
 import pt.up.fe.specs.util.SpecsIo;
 
 public class RiscvTraceAnalysisTest {
@@ -29,7 +25,7 @@ public class RiscvTraceAnalysisTest {
 //                "org/specs/Riscv/asm/" + elfname + ".elf", RiscvTraceStream.class);
 //        System.out.println("OK!");
 //    }
-    
+//    
     private DetailedRegisterInstructionProducer getProducer(String elfname) {
         String path = "org/specs/Riscv/asm/" + elfname + ".elf";
         File fd = SpecsIo.resourceCopy(path);
@@ -38,33 +34,33 @@ public class RiscvTraceAnalysisTest {
         DetailedRegisterInstructionProducer tr = new RiscvDetailedTraceProvider(fd);
         return tr;
     }
-    
-    @Test
-    public void testRegisters() throws IOException {
-        DetailedRegisterInstructionProducer tr = getProducer("example");
-
-        RegisterDump rd = tr.nextRegister();
-        Instruction inst = tr.nextInstruction();
-        rd.prettyPrint();
-        System.out.println("\n");
-        inst.printInstruction();
-        System.out.println("\n");
-        assertFalse(rd.isEmpty());
-    }
-    
-    @Test
-    public void testRegistersInInst() throws IOException {
-        DetailedRegisterInstructionProducer tr = getProducer("example");
-
-        Instruction inst = tr.nextInstruction();
-        assertFalse(inst.getRegisters().isEmpty());
-    }
+//    
+//    @Test
+//    public void testRegisters() throws IOException {
+//        DetailedRegisterInstructionProducer tr = getProducer("example");
+//
+//        RegisterDump rd = tr.nextRegister();
+//        Instruction inst = tr.nextInstruction();
+//        rd.prettyPrint();
+//        System.out.println("\n");
+//        inst.printInstruction();
+//        System.out.println("\n");
+//        assertFalse(rd.isEmpty());
+//    }
+//    
+//    @Test
+//    public void testRegistersInInst() throws IOException {
+//        DetailedRegisterInstructionProducer tr = getProducer("example");
+//
+//        Instruction inst = tr.nextInstruction();
+//        assertFalse(inst.getRegisters().isEmpty());
+//    }
     
     @Test
     public void testAnalyzer() {
         DetailedRegisterInstructionProducer tr = getProducer("example");
         
         TraceAnalysis ta = new TraceAnalysis(tr);
-        ta.analyze();
+        assertTrue(ta.analyze());
     }
 }
