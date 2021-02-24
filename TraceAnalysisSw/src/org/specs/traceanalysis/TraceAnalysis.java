1bdca31f2eaf8bf58a605a4e46f40d18362ab22c
tiagolascasas <tiagolascasas@hotmail.com>
2021-02-24 17:39:12 +0000

    [TraceAnalysisSw] Adds new project for trace analysis in software

---
+++ TraceAnalysisSw/src/org/specs/traceanalysis/TraceAnalysis.java
@@ -0,0 +1,29 @@
+package org.specs.traceanalysis;
+
+import pt.up.fe.specs.binarytranslation.producer.detailed.DetailedRegisterInstructionProducer;
+
+public class TraceAnalysis {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private DetailedRegisterInstructionProducer provider;
+    
+    public TraceAnalysis(DetailedRegisterInstructionProducer provider) {
+        this.setProvider(provider);
+    }
+
+    public DetailedRegisterInstructionProducer getProvider() {
+        return provider;
+    }
+
+    public void setProvider(DetailedRegisterInstructionProducer provider) {
+        this.provider = provider;
+    }
+
+    public boolean analyze() {
+        return true;
+    }
+}
