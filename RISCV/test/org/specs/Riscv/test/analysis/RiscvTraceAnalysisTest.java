877a705b6be7af07f71b8b07ca02f426b775626f
tiagolascasas <tiagolascasas@hotmail.com>
2021-02-24 17:37:51 +0000

    [RISCV] Adds new tests for registers using RISC-V

--- RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
+++ RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
@@ -1,19 +1,17 @@
 package org.specs.Riscv.test.analysis;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import org.junit.Test;
-import org.specs.Riscv.stream.RiscvTraceStream;
 import org.specs.Riscv.stream.detailed.RiscvDetailedTraceProvider;
-import org.specs.Riscv.stream.detailed.RiscvDetailedTraceStream;
+import org.specs.traceanalysis.TraceAnalysis;
 
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
+import pt.up.fe.specs.binarytranslation.producer.detailed.DetailedRegisterInstructionProducer;
 import pt.up.fe.specs.binarytranslation.producer.detailed.RegisterDump;
-import pt.up.fe.specs.binarytranslation.test.stream.InstructionStreamTestUtils;
 import pt.up.fe.specs.util.SpecsIo;
 
 public class RiscvTraceAnalysisTest {
@@ -32,26 +30,41 @@ public class RiscvTraceAnalysisTest {
 //        System.out.println("OK!");
 //    }
     
-    @Test
-    public void testRegisters() throws IOException {
-        // extensionless name of the elf to run
-        String elfname = "example";
-        // true to print trace to std; false to print to a txt file instead
-        boolean std = true;
-        
+    private DetailedRegisterInstructionProducer getProducer(String elfname) {
         String path = "org/specs/Riscv/asm/" + elfname + ".elf";
         File fd = SpecsIo.resourceCopy(path);
         fd.deleteOnExit();
         
-        RiscvDetailedTraceProvider test = new RiscvDetailedTraceProvider(fd);
+        DetailedRegisterInstructionProducer tr = new RiscvDetailedTraceProvider(fd);
+        return tr;
+    }
+    
+    @Test
+    public void testRegisters() throws IOException {
+        DetailedRegisterInstructionProducer tr = getProducer("example");
 
-        for (int i = 0; i < 100; i++) {
-            RegisterDump rd = test.nextRegister();
-            Instruction inst = test.nextInstruction();
-            rd.prettyPrint();
-            System.out.println("\n");
-            inst.printInstruction();
-            System.out.println("\n");
-        }
+        RegisterDump rd = tr.nextRegister();
+        Instruction inst = tr.nextInstruction();
+        rd.prettyPrint();
+        System.out.println("\n");
+        inst.printInstruction();
+        System.out.println("\n");
+        assertFalse(rd.isEmpty());
+    }
+    
+    @Test
+    public void testRegistersInInst() throws IOException {
+        DetailedRegisterInstructionProducer tr = getProducer("example");
+
+        Instruction inst = tr.nextInstruction();
+        assertFalse(inst.getRegisters().isEmpty());
+    }
+    
+    @Test
+    public void testAnalyzer() {
+        DetailedRegisterInstructionProducer tr = getProducer("example");
+        
+        TraceAnalysis ta = new TraceAnalysis(tr);
+        ta.analyze();
     }
 }
