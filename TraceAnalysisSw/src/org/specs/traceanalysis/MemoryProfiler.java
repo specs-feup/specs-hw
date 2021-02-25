521f3c9dd7f82fc8f907d7b64e9b024c91aa20f0
tiagolascasas <tiagolascasas@outlook.com>
2021-02-25 19:03:03 +0000

    [TraceAnalysisSw] Improved reporting of accessed memory addresses

--- TraceAnalysisSw/src/org/specs/traceanalysis/MemoryProfiler.java
+++ TraceAnalysisSw/src/org/specs/traceanalysis/MemoryProfiler.java
@@ -42,25 +42,37 @@ public class MemoryProfiler {
 
         var table = buildHistogram();
         
+        printHistogram(table);
+    }
+
+    private void printHistogram(HashMap<Long, Integer[]> table) {
         List<Long> sortedAddr = new ArrayList<Long>(table.keySet());
         Collections.sort(sortedAddr);
 
-        System.out.println("\nAddress  | Loads | Stores");
+        System.out.println("\nAddress | Ld | St");
+        System.out.println("-------------------");
         for (Long addr : sortedAddr) {
             Integer[] counts = table.get(addr);
-            System.out.println(addr + " |" + counts[0] + " |" + counts[1]);
+            String strAddr = String.format("%-8s", addr);
+            String strCnt0 = String.format("%-4s", counts[0]);
+            String strCnt1 = String.format("%-4s", counts[1]);
+            
+            System.out.println(strAddr + "|" + strCnt0 + "|" + strCnt1);
         }
+        System.out.println("-------------------");
     }
 
     public void prettyPrint() {
+        System.out.println("Detected load/store operations (val. in decimal)");
+        System.out.println("---------------------------------------");
         Queue<Instruction> newq = new LinkedList<>();
         Instruction inst = queue.poll();
         while (inst != null) {
-            //System.out.println(inst.getData().getOperands());
             printInstWithRegs(inst);
             newq.add(inst);
             inst = queue.poll();
         }
+        System.out.println("---------------------------------------");
         this.queue = newq;
     }
     
@@ -86,14 +98,20 @@ public class MemoryProfiler {
 
     private void printInstWithRegs(Instruction inst) {
         StringBuilder sb = new StringBuilder();
-        sb.append(inst.getRepresentation()).append("|  ");
+        String space = "  ";
+
+        sb.append(String.format("%-4s", inst.getName())).append(space);
         
         for (Operand op : inst.getData().getOperands()) {
             if (op.isRegister()) {
-                String reg = "r" + op.getStringValue();
+                String reg = op.getProperties().getPrefix() + op.getStringValue();
                 Long res = inst.getRegisters().getValue(reg);
                 String regVal = res == null ? "??" : res.toString();
-                sb.append(reg).append("{").append(regVal).append("} ");
+                sb.append(String.format("%-12s", reg + "{" + regVal + "}")).append(space);
+            }
+            if (op.isImmediate()) {
+                long imm = Long.parseLong(op.getStringValue(), 16);
+                sb.append(String.format("%-8s", imm)).append(space);
             }
         }
         System.out.println(sb.toString());
@@ -103,18 +121,18 @@ public class MemoryProfiler {
      * Calculates the address of a load/store instruction
      * Supports offsets from both other registers and immediate values
      * MicroBlaze-specific for now
-     * @param inst
-     * @return
+     * @param inst a load/store instruction
+     * @return the memory address being used by the instruction
      */
     private Long getMemoryAddr(Instruction inst) {
         Operand op = inst.getData().getOperands().get(1);
-        long res = inst.getRegisters().getValue("r" + op.getStringValue());
+        long res = inst.getRegisters().getValue(op.getProperties().getPrefix() + op.getStringValue());
         
         if (inst.getData().getOperands().size() == 3) {
             op = inst.getData().getOperands().get(2);
             long add = 0;
             if (op.isRegister()) {
-                add = inst.getRegisters().getValue("r" + op.getStringValue());
+                add = inst.getRegisters().getValue(op.getProperties().getPrefix() + op.getStringValue());
             }
             if (op.isImmediate()) {
                 add = Long.parseLong(op.getStringValue(), 16);
