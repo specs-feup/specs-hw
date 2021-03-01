4314a08a9d3250cdcf4cb474b9a186ed14d465bb
tiagolascasas <tiagolascasas@outlook.com>
2021-03-01 19:44:16 +0000

    [MicroBlaze] Adds tests for the new analyzers

--- MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceStream.java
+++ MicroBlaze/src/org/specs/MicroBlaze/stream/MicroBlazeTraceStream.java
@@ -1,12 +1,14 @@
 package org.specs.MicroBlaze.stream;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 
 import org.specs.MicroBlaze.asm.MicroBlazeApplication;
 import org.specs.MicroBlaze.asm.MicroBlazeELFDump;
 
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.producer.ChanneledInstructionProducer;
+import pt.up.fe.specs.binarytranslation.producer.TraceInstructionProducer;
 import pt.up.fe.specs.binarytranslation.stream.ATraceInstructionStream;
 import pt.up.fe.specs.util.SpecsIo;
 import pt.up.fe.specs.util.collections.concurrentchannel.ChannelConsumer;
@@ -57,6 +59,16 @@ public class MicroBlazeTraceStream extends ATraceInstructionStream {
 
     public MicroBlazeTraceStream(File elfname) {
         super(new MicroBlazeTraceProvider(elfname));
+        this.elfdump = MicroBlazeTraceStream.GDBbugHandle(elfname);
+    }
+    
+    /**
+     * Use a custom trace provider instead of the default
+     * @param prod an initialized custom trace provider
+     */
+    public MicroBlazeTraceStream(TraceInstructionProducer prod) {
+        super(prod);
+        File elfname = prod.getApp().getElffile();
         this.elfdump = MicroBlazeTraceStream.GDBbugHandle(elfname);
     }
 
