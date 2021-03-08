9d090c88509ddf446c90fde6b15fac593bcf2947
tiagolascasas <tiagolascasas@outlook.com>
2021-03-08 18:35:03 +0000

    [RISCV] Adds tests and examples for RISC-V basic block in/out analysis

--- RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
+++ RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
@@ -1,19 +1,38 @@
 package org.specs.Riscv.test.analysis;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.specs.BinaryTranslation.ELFProvider;
+import org.specs.Riscv.RiscvGccELF;
 import org.specs.Riscv.RiscvLivermoreELFN100iam;
 import org.specs.Riscv.stream.RiscvDetailedTraceProvider;
+import org.specs.Riscv.stream.RiscvTraceStream;
 
-import pt.up.fe.specs.binarytranslation.analysis.TraceAnalysis;
+import pt.up.fe.specs.binarytranslation.analysis.basicblock.InOutAnalysis;
 import pt.up.fe.specs.binarytranslation.utils.BinaryTranslationUtils;
 
 public class RiscvTraceAnalysisTest {
 
+    private RiscvTraceStream getStream(ELFProvider file) {
+        var fd = BinaryTranslationUtils.getFile(file);
+        RiscvDetailedTraceProvider prod = new RiscvDetailedTraceProvider(fd);
+        var stream = new RiscvTraceStream(prod);
+        return stream;
+    }
+
+    
     @Test
     public void testRiscVAnalysis() {
         var fd = BinaryTranslationUtils.getFile(RiscvLivermoreELFN100iam.innerprod100);
         var dip = new RiscvDetailedTraceProvider(fd);
+    }
+    
+    @Test
+    public void testRiscvInOuts() {
+        //var elf = RiscvLivermoreELFN100iam.innerprod100;
+        var elf = RiscvGccELF.autocor;
+        var stream = getStream(elf);
+        
+        InOutAnalysis bba = new InOutAnalysis(stream);
+        bba.analyse();
     }
 }
