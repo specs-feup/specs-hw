98fdb4ada3bcfae8ef884da1aedb80b58ed1b94e
tiagolascasas <tiagolascasas@outlook.com>
2021-03-09 18:24:36 +0000

    [BinaryTranslation] Improves in/out calculation for traces

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/analysis/inouts/TraceInOuts.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/analysis/inouts/TraceInOuts.java
@@ -15,6 +15,31 @@ public class TraceInOuts extends ASequenceInOuts {
     }
 
     @Override
+    public void printSequenceInOuts() {
+        for (BinarySegment bb : bbs) {
+            long start = bb.getInstructions().get(0).getAddress();
+            long end = bb.getInstructions().get(bb.getInstructions().size() - 1).getAddress();
+            boolean expectingEnd = false;
+            
+            System.out.println("Displaying in/outs for all occurrences of basic block");
+            System.out.println("-----------------------------------------------------");
+            for (int i = 0; i < insts.size(); i++) {
+                Instruction inst = insts.get(i);
+                if (inst.getAddress() == start && !expectingEnd) {
+                    printInstWithSets(i);
+                    System.out.println("[basic block body...]");
+                    expectingEnd = true;
+                }
+                if (inst.getAddress() == end && expectingEnd) {
+                    printInstWithSets(i);
+                    expectingEnd = false;
+                    System.out.println("-----------------------------------------------------");
+                }
+            }
+        }
+    }
+
+    @Override
     public void printResult() {
         // TODO Auto-generated method stub
 
