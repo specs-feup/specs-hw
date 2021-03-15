7e243852bc1ddd771b94553ab6ed339cad34c080
tiagolascasas <tiagolascasas@outlook.com>
2021-03-15 17:52:49 +0000

    [RISCV] Updates RISC-V for new .txt traces

--- RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
+++ RISCV/test/org/specs/Riscv/test/analysis/RiscvTraceAnalysisTest.java
@@ -1,5 +1,7 @@
 package org.specs.Riscv.test.analysis;
 
+import java.io.File;
+
 import org.junit.Test;
 import org.specs.BinaryTranslation.ELFProvider;
 import org.specs.Riscv.RiscvGccELF;
@@ -8,18 +10,24 @@ import org.specs.Riscv.stream.RiscvDetailedTraceProvider;
 import org.specs.Riscv.stream.RiscvTraceStream;
 
 import pt.up.fe.specs.binarytranslation.analysis.InOutAnalysis;
+import pt.up.fe.specs.binarytranslation.analysis.InOutMode;
 import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.utils.BinaryTranslationUtils;
+import pt.up.fe.specs.util.SpecsIo;
 
 public class RiscvTraceAnalysisTest {
 
-    private RiscvTraceStream getStream(ELFProvider file) {
-        var fd = BinaryTranslationUtils.getFile(file);
-        RiscvDetailedTraceProvider prod = new RiscvDetailedTraceProvider(fd);
+    private RiscvTraceStream getStream(ELFProvider file, boolean generate) {
+        String f = file.getResource();
+        if (!generate) {
+            f = ((RiscvLivermoreELFN100iam) file).asTraceTxtDump();
+        }
+        File fd = SpecsIo.resourceCopy(f);
+        fd.deleteOnExit();
+        var prod = new RiscvDetailedTraceProvider(fd);
         var stream = new RiscvTraceStream(prod);
         return stream;
     }
-
     
     @Test
     public void testRiscvDetailedProvider() {
@@ -37,9 +45,9 @@ public class RiscvTraceAnalysisTest {
     public void testRiscvInOuts() {
         //var elf = RiscvLivermoreELFN100iam.innerprod100;
         var elf = RiscvGccELF.autocor;
-        var stream = getStream(elf);
+        var stream = getStream(elf, true);
         
         InOutAnalysis bba = new InOutAnalysis(stream);
-        bba.analyse();
+        bba.analyse(InOutMode.BASIC_BLOCK);
     }
 }
