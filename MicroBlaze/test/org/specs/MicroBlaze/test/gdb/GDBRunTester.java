457853725385649d0a6ee169254726619ba4ce80
nmcp88 <nmcp88@gmail.com>
2021-03-16 17:09:08 +0000

    [MicroBlaze] testing interaction with GDB in interactive mode

--- MicroBlaze/test/org/specs/MicroBlaze/test/gdb/GDBRunTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/gdb/GDBRunTester.java
@@ -9,6 +9,23 @@ import pt.up.fe.specs.binarytranslation.utils.BinaryTranslationUtils;
 
 public class GDBRunTester {
 
+    /*
+     * set confirm off
+        undisplay
+        set print address off
+        set height 0
+        file <ELFNAME>
+        
+        
+    MB:        target remote | <QEMUBIN> -nographic -M microblaze-fdt-plnx 
+    -m 64 -display none -kernel <ELFNAME> -dtb <DTBFILE> -chardev stdio,mux=on,id=char0 
+    -mon chardev=char0,mode=readline -serial chardev:char0 -gdb chardev:char0 -S
+    
+    ARM        target remote | <QEMUBIN> -M virt -cpu cortex-a53 -nographic 
+    -kernel <ELFNAME> -chardev stdio,mux=on,id=char0 
+    -mon chardev=char0,mode=readline -serial chardev:char0 -gdb chardev:char0 -S
+     */
+
     @Test
     public void test() {
         var fd = BinaryTranslationUtils.getFile(MicroBlazeLivermoreELFN10.innerprod);
@@ -16,22 +33,31 @@ public class GDBRunTester {
 
         try (var gdb = new GDBRun(app)) {
 
-            String line = null;
-            while ((line = gdb.getGDBResponse()) != null) {
-                System.out.println(line);
+            gdb.loadFile(app);
+
+            // copy dtb to local folder
+            var dtb = BinaryTranslationUtils.getFile(app.getDtbfile().getResource());
+            dtb.deleteOnExit();
+
+            var remoteCommand = app.getQemuexe().getResource()
+                    + " -nographic -M microblaze-fdt-plnx -m 64 -display none"
+                    + " -kernel " + app.getElffile().getAbsolutePath()
+                    + " -dtb " + dtb.getAbsolutePath()
+                    + " -chardev stdio,mux=on,id=char0"
+                    + " -mon chardev=char0,mode=readline -serial chardev:char0 -gdb chardev:char0 -S";
+
+            gdb.launchTarget(remoteCommand);
+            for (int i = 0; i < 10; i++) {
+                gdb.stepi();
+                System.out.println(gdb.getAddrAndInstruction());
+                // System.out.println(gdb.getRegisters());
             }
 
-            gdb.setupForTracing();
-            while ((line = gdb.getGDBResponse()) != null) {
-                System.out.println(line);
-            }
+            // var variables = gdb.getVariableList();
+            // System.out.println(variables);
 
-            gdb.sendGDBCommand("quit");
-            gdb.close();
-
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            // kill target and quit gdb
+            gdb.quit();
         }
     }
 }
