216bddfe865d0f51ab7efdd41947f01308d56958
tiagolascasas <tiagolascasas@outlook.com>
2021-03-17 17:50:52 +0000

    [BinaryTranslation] Simplified occurrence detection in trace

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/analysis/occurrence/BasicBlockOccurrenceTracker.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/analysis/occurrence/BasicBlockOccurrenceTracker.java
@@ -1,22 +1,38 @@
 package pt.up.fe.specs.binarytranslation.analysis.occurrence;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import pt.up.fe.specs.binarytranslation.detection.segments.BinarySegment;
+import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 
 public class BasicBlockOccurrenceTracker {
     
     private BinarySegment bb;
     private ArrayList<BasicBlockOccurrence> occurrences;
-
-    public BasicBlockOccurrenceTracker(BinarySegment bb, ArrayList<Integer> positions) {
+    
+    public BasicBlockOccurrenceTracker(BinarySegment bb, List<Instruction> insts) {
         this.bb = bb;
         this.occurrences = new ArrayList<>();
+        
         int id = 0;
-        for (Integer i : positions) {
-            var o = new BasicBlockOccurrence(id, bb, i);
-            occurrences.add(o);
-            id++;
+        long start = bb.getInstructions().get(0).getAddress();
+        long end = bb.getInstructions().get(bb.getInstructions().size() - 1).getAddress();
+        boolean expectingEnd = false;
+        int newIdx = 0;
+        
+        for (int i = 0; i < insts.size(); i++) {
+            Instruction inst = insts.get(i);
+            if (inst.getAddress() == start && !expectingEnd) {
+                expectingEnd = true;
+                newIdx = i;
+            }
+            if (inst.getAddress() == end && expectingEnd) {
+                expectingEnd = false;
+                var newOccurrence = new BasicBlockOccurrence(id, bb, newIdx);
+                occurrences.add(newOccurrence);
+                id++;
+            }
         }
     }
 
