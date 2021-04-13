6bef91daae66e670f4e72440d03c01dd737df609
tiagolascasas <tiagolascasas@outlook.com>
2021-04-13 20:10:59 +0100

    [BinaryTranslation] Adds interface for register comparisons

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/asm/AIsaRegisterConventions.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/asm/AIsaRegisterConventions.java
@@ -3,7 +3,7 @@ package pt.up.fe.specs.binarytranslation.asm;
 import java.util.Arrays;
 import java.util.List;
 
-public abstract class IsaRegisterConventions {    
+public abstract class AIsaRegisterConventions implements RegisterProperties {    
     protected List<String> parameters;
     protected List<String> temporaries;
     protected List<String> returnVals;
@@ -11,7 +11,7 @@ public abstract class IsaRegisterConventions {
     protected String zero;
     
     protected void setStackPointer(String reg) {
-        this.stackPointer = reg;
+        stackPointer = reg;
     }
     
     protected void setZero(String reg) {
@@ -48,6 +48,26 @@ public abstract class IsaRegisterConventions {
     
     public String getZero() {
         return zero;
+    }
+    
+    public boolean isStackPointer(String reg) {
+        return stackPointer.equals(reg);
+    }
+    
+    public boolean isTemporary(String reg) {
+        return temporaries.contains(reg);
+    }
+    
+    public boolean isParameter(String reg) {
+        return parameters.contains(reg);
+    }
+    
+    public boolean isZero(String reg) {
+        return zero.equals(reg);
+    }
+    
+    public boolean isReturn(String reg) {
+        return returnVals.equals(reg);
     }
 }
 
