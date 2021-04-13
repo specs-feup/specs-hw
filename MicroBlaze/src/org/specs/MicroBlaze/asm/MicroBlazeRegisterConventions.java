18c8d95f50f570353c1164812dab2f529e45c837
tiagolascasas <tiagolascasas@outlook.com>
2021-04-13 20:11:41 +0100

    [MicroBlaze] Tests passing MB register conventions to memory analyzer

--- MicroBlaze/src/org/specs/MicroBlaze/asm/MicroBlazeRegisterConventions.java
+++ MicroBlaze/src/org/specs/MicroBlaze/asm/MicroBlazeRegisterConventions.java
@@ -1,8 +1,8 @@
 package org.specs.MicroBlaze.asm;
 
-import pt.up.fe.specs.binarytranslation.asm.IsaRegisterConventions;
+import pt.up.fe.specs.binarytranslation.asm.AIsaRegisterConventions;
 
-public class MicroBlazeRegisterConventions extends IsaRegisterConventions {
+public class MicroBlazeRegisterConventions extends AIsaRegisterConventions {
 
     public MicroBlazeRegisterConventions() {
         setParameters("r5", "r6", "r7", "r8", "r9", "r10");
