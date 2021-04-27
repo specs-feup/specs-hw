76a525cfd2a37e2bd9f4044287fc5e55c2d95781
nmcp88 <nmcp88@gmail.com>
2021-04-27 16:39:40 +0100

    [BinaryTranslation] using JGraphT to create TraceUnit graphs

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/tracer/StaticGraphGenerator.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/tracer/StaticGraphGenerator.java
@@ -4,27 +4,55 @@ import pt.up.fe.specs.binarytranslation.stream.InstructionStream;
 
 public class StaticGraphGenerator {
 
+    /*
+     * Using TreeNode
+     
     public static TraceUnitGraph generateStaticGraph(InstructionStream istream) {
-
+    
         // basic blocks
         var tracer = new StreamTracer(istream);
-
+    
         // head
         var head = new TraceGraphNode(tracer.nextBasicBlock());
         var tgraph = new TraceUnitGraph(head);
-
+    
         int i = 50; // just for testing!
         while (tracer.hasNext()) {
-
+    
             // next block
             var next = tracer.nextBasicBlock();
             System.out.println(next);
             tgraph.insert(new TraceGraphNode(next));
-
+    
             // count down
             i--;
             if (i == 0)
                 break;
+        }
+    
+        return tgraph;
+    }*/
+
+    /*
+     * Using JGraphT
+     */
+    public static TraceUnitGraphT generateStaticGraph(InstructionStream istream) {
+
+        // basic blocks
+        var tracer = new StreamTracer(istream);
+        var tgraph = new TraceUnitGraphT();
+
+        // int i = 50; // just for testing!
+        while (tracer.hasNext()) {
+
+            // next block
+            var next = tracer.nextBasicBlock();
+            tgraph.insert(next);
+
+            // count down
+            // i--;
+            // if (i == 0)
+            // break;
         }
 
         return tgraph;
