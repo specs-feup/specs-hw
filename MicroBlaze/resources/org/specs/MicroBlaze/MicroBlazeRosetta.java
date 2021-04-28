d35443f5e992c4bae369b33c74b7f498a4c8ea30
tiagolascasas <tiagolascasas@outlook.com>
2021-04-28 19:59:48 +0100

    [MicroBlaze] Adds resource provider for Rosetta apps

---
+++ MicroBlaze/resources/org/specs/MicroBlaze/MicroBlazeRosetta.java
@@ -0,0 +1,48 @@
+package org.specs.MicroBlaze;
+
+import org.specs.BinaryTranslation.ELFProvider;
+
+public enum MicroBlazeRosetta implements ELFProvider {
+
+    rendering3d("org/specs/MicroBlaze/asm/Rosetta/3d-rendering.elf", null, null),
+    facedetection("org/specs/MicroBlaze/asm/Rosetta/face-detection.elf", null, null);
+
+    // private String elfname;
+    private String fullPath;
+    private Number kernelStart;
+    private Number kernelStop;
+
+    private MicroBlazeRosetta(String fullPath, Number kernelStart, Number kernelStop) {
+        // this.elfname = name();
+        this.fullPath = fullPath;
+        this.kernelStart = kernelStart;
+        this.kernelStop = kernelStop;
+    }
+
+    @Override
+    public String getResource() {
+        return this.fullPath;
+    }
+
+    @Override
+    public String asTxtDump() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Number getKernelStart() {
+        return this.kernelStart;
+    }
+
+    @Override
+    public Number getKernelStop() {
+        return this.kernelStop;
+    }
+
+    @Override
+    public String asTraceTxtDump() {
+        return this.fullPath.replace(".elf", "_trace.txt");
+    }
+
+}
