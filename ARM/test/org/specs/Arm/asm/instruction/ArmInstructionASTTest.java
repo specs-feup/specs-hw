4eb6cee770c85985f8428b8f85879837f0ea74af
nmcp88 <nmcp88@gmail.com>
2021-04-30 15:12:53 +0100

    [ARM] added G_RJUMP or G_AJUMP property to some insts (was missing)

--- ARM/test/org/specs/Arm/asm/instruction/ArmInstructionASTTest.java
+++ ARM/test/org/specs/Arm/asm/instruction/ArmInstructionASTTest.java
@@ -8,8 +8,9 @@ import pt.up.fe.specs.binarytranslation.instruction.ast.InstructionAST;
 import pt.up.fe.specs.binarytranslation.instruction.ast.passes.ApplyInstructionPass;
 import pt.up.fe.specs.binarytranslation.instruction.ast.passes.ApplySSAPass;
 import pt.up.fe.specs.binarytranslation.lex.listeners.TreeDumper;
+import pt.up.fe.specs.util.treenode.utils.DottyGenerator;
 
-public class ArmParseTreeTester {
+public class ArmInstructionASTTest {
 
     // DPI_ADDSUBIMM (C4-253 to C4-254)
 
@@ -42,9 +43,18 @@ public class ArmParseTreeTester {
         // 0x12b8: 11000422 add w2, w1, #0x1
         var addi = ArmInstruction.newInstance("12b8", "11000422");
         var ast = new InstructionAST(addi);
+        var dotty1 = DottyGenerator.buildDotty(ast.getRootnode());
+        System.out.println(dotty1);
+
         ast.accept(new ApplyInstructionPass());
+        var dotty2 = DottyGenerator.buildDotty(ast.getRootnode());
+        System.out.println(dotty2);
+
         ast.accept(new ApplySSAPass());
-        System.out.println(ast.getRootnode().getAsString());
+        var dotty3 = DottyGenerator.buildDotty(ast.getRootnode());
+        System.out.println(dotty3);
+
+        // System.out.println(ast.getRootnode().getAsString());
     }
 
 }
