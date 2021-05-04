a25a2e465855f3da74319dd930901fafad83cae6
Tiago <tiagolascasas@outlook.com>
2021-05-04 18:18:26 +0100

    [MicroBlaze] Adds list of registers to RegisterProperties

--- MicroBlaze/src/org/specs/MicroBlaze/asm/MicroBlazeRegisterConventions.java
+++ MicroBlaze/src/org/specs/MicroBlaze/asm/MicroBlazeRegisterConventions.java
@@ -5,6 +5,10 @@ import pt.up.fe.specs.binarytranslation.asm.AIsaRegisterConventions;
 public class MicroBlazeRegisterConventions extends AIsaRegisterConventions {
 
     public MicroBlazeRegisterConventions() {
+        setAllRegisters("r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", 
+                        "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18", "r19", "r20",
+                        "r21", "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", "r30",
+                        "r31", "rpc");
         setParameters("r5", "r6", "r7", "r8", "r9", "r10");
         setTemporaries("r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12");
         setReturnVals("r3", "r4");
@@ -12,4 +16,3 @@ public class MicroBlazeRegisterConventions extends AIsaRegisterConventions {
         setZero("r0");
     }
 }
-
