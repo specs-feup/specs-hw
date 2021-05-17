f2ceaec17483e3976e169a6fbff8f691689778d9
Tiago <tiagolascasas@outlook.com>
2021-05-17 19:58:44 +0100

    [BinaryTranslation] Changes to kernel memory access types analyzer

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/analysis/MemoryAccessTypesAnalyzer.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/analysis/MemoryAccessTypesAnalyzer.java
@@ -13,8 +13,13 @@
 
 package pt.up.fe.specs.binarytranslation.analysis;
 
+import java.util.List;
+
 import org.specs.BinaryTranslation.ELFProvider;
 
+import pt.up.fe.specs.binarytranslation.analysis.memory.MemoryAddressDetector;
+import pt.up.fe.specs.binarytranslation.detection.segments.BinarySegment;
+import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.stream.ATraceInstructionStream;
 
 public class MemoryAccessTypesAnalyzer extends ATraceAnalyzer {
@@ -23,10 +28,21 @@ public class MemoryAccessTypesAnalyzer extends ATraceAnalyzer {
         super(stream, elf);
     }
 
-    public void analyze() {
-        var trace = AnalysisUtils.getCompleteTrace(stream,
-                elf.getKernelStart() == null ? 0 : elf.getKernelStart().longValue(),
-                elf.getKernelStop() == null ? -1 : elf.getKernelStop().longValue());
-        System.out.println("Trace has " + trace.size() + " instructions");
+    public void analyze(int window) {
+//        var trace = AnalysisUtils.getCompleteTrace(stream,
+//                elf.getKernelStart() == null ? 0 : elf.getKernelStart().longValue(),
+//                elf.getKernelStop() == null ? -1 : elf.getKernelStop().longValue());
+//        System.out.println("Trace has " + trace.size() + " instructions");
+        
+        var det = buildDetector(window);
+        List<BinarySegment> segs = AnalysisUtils.getSegments(stream, det);
+        List<Instruction> insts = det.getProcessedInsts();
+
+        for (var bb : segs) {
+            var mad = new MemoryAddressDetector(bb, insts);
+            var graphs = mad.detectGraphs();
+
+            MemoryAddressAnalyzer.printExpressions(graphs);
+        }
     }
 }
