41954a2fe5587f91ba053c187dbba8867eeac14b
João Bispo <joaobispo@gmail.com>
2021-07-16 13:41:36 +0100

    [BinaryTranslation] Restores some legacy files

---
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/legacy/TraceInstruction32.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2011 SPeCS Research Group.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+package pt.up.fe.specs.binarytranslation.legacy;
+
+import java.util.Optional;
+
+import pt.up.fe.specs.util.exceptions.NotImplementedException;
+
+/**
+ * Represents an executed instruction : a SimpleInstructrion and the number of cycles that instruction needed to
+ * execute.
+ * 
+ * @author Joao Bispo
+ */
+public interface TraceInstruction32 extends SimpleInstruction32 {
+
+    /**
+     * How many cycles it takes to execute.
+     * 
+     * @return
+     */
+    Integer getCycles();
+
+    // public final Integer instructionCycles;
+
+    /**
+     * True if the current instruction can signal a possible exit.
+     * 
+     * @return
+     */
+    @Override
+    default boolean isJump() {
+        throw new NotImplementedException(this);
+    }
+
+    /**
+     * 
+     * @return as default, returns empty
+     */
+    default Optional<Integer> getEncoded() {
+        return Optional.empty();
+    }
+}
