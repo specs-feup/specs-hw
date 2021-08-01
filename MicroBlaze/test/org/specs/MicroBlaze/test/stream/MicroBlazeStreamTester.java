e29318f89a6335e6852a1e214e99f3da22fed685
nmcp88 <nmcp88@gmail.com>
2021-08-01 22:12:45 +0100

    [MicroBlaze] updated Elfprovider paths, and some changes in testers

--- MicroBlaze/test/org/specs/MicroBlaze/test/stream/MicroBlazeStreamTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/stream/MicroBlazeStreamTester.java
@@ -1,16 +1,11 @@
 package org.specs.MicroBlaze.test.stream;
 
-import java.io.File;
-
 import org.junit.Test;
-import org.specs.MicroBlaze.MicroBlazeLivermoreELFN10;
 import org.specs.MicroBlaze.MicroBlazePolyBenchSmallFloat;
 import org.specs.MicroBlaze.stream.MicroBlazeElfStream;
 import org.specs.MicroBlaze.stream.MicroBlazeTraceStream;
 
-import pt.up.fe.specs.binarytranslation.instruction.Instruction;
 import pt.up.fe.specs.binarytranslation.test.stream.InstructionStreamTestUtils;
-import pt.up.fe.specs.util.SpecsIo;
 
 /**
  * Trace printing test cases; txt files are used so that the backend tools can run on Jenkins without need for
@@ -42,28 +37,10 @@ public class MicroBlazeStreamTester {
         // MicroBlazeLivermoreELFN10.innerprod, MicroBlazeTraceStream.class);
     }
 
-    // TODO: rawDump now doesnt work if the qemu template doesnt include the loop...
     @Test
     public void testTraceRaw() {
         InstructionStreamTestUtils.rawDump(
                 MicroBlazePolyBenchSmallFloat.gemm, MicroBlazeTraceStream.class);
         // MicroBlazeLivermoreELFN10.innerprod, MicroBlazeTraceStream.class);
-    }
-
-    @Test
-    public void test() {
-
-        File fd = SpecsIo.resourceCopy(MicroBlazeLivermoreELFN10.innerprod.getResource());
-        fd.deleteOnExit();
-
-        try (var stream = new MicroBlazeElfStream(fd)) {
-            Instruction inst = null;
-            while ((inst = stream.nextInstruction()) != null) {
-                var op = inst.getData().getOperands();
-                for (var ope : op) {
-                    System.out.println(ope.getAsmField().toString());
-                }
-            }
-        }
     }
 }
