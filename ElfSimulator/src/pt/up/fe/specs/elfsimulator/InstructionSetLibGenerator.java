bbb7de494bf544d10653cfd21504821dc1829790
Nuno <nmcp88@gmail.com>
2021-08-02 13:27:30 +0100

    [ElfSimulator] fixed some compile errors on this stale project

--- ElfSimulator/src/pt/up/fe/specs/elfsimulator/InstructionSetLibGenerator.java
+++ ElfSimulator/src/pt/up/fe/specs/elfsimulator/InstructionSetLibGenerator.java
@@ -1,18 +1,14 @@
 /**
- *  Copyright 2020 SPeCS.
+ * Copyright 2020 SPeCS.
  * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
  */
 
 package pt.up.fe.specs.elfsimulator;
@@ -25,48 +21,47 @@ import pt.up.fe.specs.binarytranslation.instruction.ast.InstructionAST;
 import pt.up.fe.specs.binarytranslation.instruction.ast.nodes.InstructionASTNode;
 import pt.up.fe.specs.binarytranslation.instruction.operand.Operand;
 import pt.up.fe.specs.binarytranslation.stream.AStaticInstructionStream;
-import pt.up.fe.specs.binarytranslation.utils.TreeNode;
 
 public class InstructionSetLibGenerator {
     public InstructionSetLibGenerator(AStaticInstructionStream stream) {
-        
-            List<Operand> uniqueOperands = new ArrayList<Operand>();
-            System.out.print(stream.getNumInstructions() + "\n" );
-            int unknownCounter = 0;
-            Instruction instruction = stream.nextInstruction();
-            while(instruction != null) {
-                System.out.print(instruction.getRepresentation() +"\n");
-                //System.out.print(instruction.getName() + "\n");
-                if(instruction.isUnknown()) 
-                    unknownCounter++;
-                else {
-                    for(Operand operand : instruction.getData().getOperands())
-                        if(!uniqueOperands.contains(operand)) uniqueOperands.add(operand);
-                    //System.out.print(instruction.getRepresentation() + "\n");
-                    //System.out.print("\t" + instruction.getFieldData().getType() + "\n");
-                    //System.out.print("\t" + instruction.getFieldData().getFields() + "\n\n");
-                        InstructionAST ast = new InstructionAST(instruction);
-                        //System.out.println(TreeNode.toString(ast.getRootnode(), "  "));
-                        
-                        //recursiveNodePrint(ast.getRootnode(), 0);       
-                }
-                instruction = stream.nextInstruction();
+
+        List<Operand> uniqueOperands = new ArrayList<Operand>();
+        System.out.print(stream.getNumInstructions() + "\n");
+        int unknownCounter = 0;
+        Instruction instruction = stream.nextInstruction();
+        while (instruction != null) {
+            System.out.print(instruction.getRepresentation() + "\n");
+            // System.out.print(instruction.getName() + "\n");
+            if (instruction.isUnknown())
+                unknownCounter++;
+            else {
+                for (Operand operand : instruction.getData().getOperands())
+                    if (!uniqueOperands.contains(operand))
+                        uniqueOperands.add(operand);
+                // System.out.print(instruction.getRepresentation() + "\n");
+                // System.out.print("\t" + instruction.getFieldData().getType() + "\n");
+                // System.out.print("\t" + instruction.getFieldData().getFields() + "\n\n");
+                InstructionAST ast = new InstructionAST(instruction);
+                // System.out.println(TreeNode.toString(ast.getRootnode(), " "));
+
+                // recursiveNodePrint(ast.getRootnode(), 0);
             }
-            System.out.print("UNIQUE OPERANDS: ");
-            for(Operand operand : uniqueOperands) System.out.print("\t" + operand.getRepresentation() + "\n");
-            System.out.print("UNKNOWN INSTRUCTIONS = " + unknownCounter + "\n");
-            System.out.print(stream.getNumInstructions() + "\n");
+            instruction = stream.nextInstruction();
+        }
+        System.out.print("UNIQUE OPERANDS: ");
+        for (Operand operand : uniqueOperands)
+            System.out.print("\t" + operand.getRepresentation() + "\n");
+        System.out.print("UNKNOWN INSTRUCTIONS = " + unknownCounter + "\n");
+        System.out.print(stream.getNumInstructions() + "\n");
     }
-   
-    
-    
-    
+
     private void recursiveNodePrint(InstructionASTNode node, int indentation) {
-        for(int i=0; i<indentation; i++) System.out.print("\t");
+        for (int i = 0; i < indentation; i++)
+            System.out.print("\t");
         System.out.print(node.getType().name() + "  ->  " + node.getAsString() + "\n");
-        for(InstructionASTNode childNode : node.getChildren()) recursiveNodePrint(childNode, ++indentation);
-            
+        for (InstructionASTNode childNode : node.getChildren())
+            recursiveNodePrint(childNode, ++indentation);
+
     }
-    
-   
+
 }
