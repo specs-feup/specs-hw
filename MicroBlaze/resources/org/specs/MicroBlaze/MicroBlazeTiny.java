5deb12664d0bde23babaf8f599005b32437f7aee
nmcp88 <nmcp88@gmail.com>
2021-08-03 01:18:25 +0100

    [MicroBlaze] added MicroBlazeTiny class

---
+++ MicroBlaze/resources/org/specs/MicroBlaze/MicroBlazeTiny.java
@@ -0,0 +1,33 @@
+package org.specs.MicroBlaze;
+
+public enum MicroBlazeTiny implements MicroBlazeELFProvider {
+
+    tiny("org/specs/MicroBlaze/asm/tiny.txt", null, null);
+
+    // private String elfname;
+    private String fullPath;
+    private Number kernelStart;
+    private Number kernelStop;
+
+    private MicroBlazeTiny(String fullPath, Number kernelStart, Number kernelStop) {
+        // this.elfname = name();
+        this.fullPath = fullPath;
+        this.kernelStart = kernelStart;
+        this.kernelStop = kernelStop;
+    }
+
+    @Override
+    public String getResource() {
+        return this.fullPath;
+    }
+
+    @Override
+    public Number getKernelStart() {
+        return this.kernelStart;
+    }
+
+    @Override
+    public Number getKernelStop() {
+        return this.kernelStop;
+    }
+}
