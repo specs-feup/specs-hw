aa24ab604f6541b385fba1411e9446a8af60cf33
nmcp88 <nmcp88@gmail.com>
2021-08-03 01:09:58 +0100

    [ElfSimulator] edit a tester due to new ELFProvider family structure

--- ElfSimulator/test/pt/up/fe/specs/elfsimulator/test/TestSimulator.java
+++ ElfSimulator/test/pt/up/fe/specs/elfsimulator/test/TestSimulator.java
@@ -1,70 +1,56 @@
 /**
- *  Copyright 2020 SPeCS.
+ * Copyright 2020 SPeCS.
  * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
  */
 
 package pt.up.fe.specs.elfsimulator.test;
 
-import java.io.File;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.specs.MicroBlaze.stream.*;
-import org.specs.Riscv.stream.RiscvElfStream;
+import org.specs.MicroBlaze.MicroBlazeLivermoreELFN10;
+import org.specs.MicroBlaze.stream.MicroBlazeElfStream;
+import org.specs.Riscv.RiscvLivermoreELFN100iam;
 
-import pt.up.fe.specs.util.SpecsIo;
-import pt.up.fe.specs.binarytranslation.instruction.InstructionData;
-import pt.up.fe.specs.binarytranslation.instruction.InstructionType;
-import pt.up.fe.specs.binarytranslation.instruction.operand.Operand;
+import pt.up.fe.specs.binarytranslation.ELFProvider;
 import pt.up.fe.specs.compiledsimulator.ExecGenerator;
 import pt.up.fe.specs.compiledsimulator.InstructionSetSpecifications;
 import pt.up.fe.specs.compiledsimulator.LibGenerator;
-import pt.up.fe.specs.elfsimulator.ElfSimulator;
-import pt.up.fe.specs.elfsimulator.InstructionSetLibGenerator;
 
 public class TestSimulator {
 
-    private File openMBFile() {
+    private ELFProvider openMBFile() {
+
+        /*File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/microblaze/tiny.txt");
+        System.out.print(fd.exists() + "\n");
+        fd.deleteOnExit();*/
+
+        return MicroBlazeLivermoreELFN10.cholesky;
+    }
+
+    private ELFProvider openRISCVFile() {
 
         // static
-        // File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/microblaze/matmul.txt");
-//         File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/microblaze/innerprod.txt");
-        File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/microblaze/tiny.txt");
-        // File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/microblaze/innerprod.txt");
-
+        /*File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/riscv/test64.txt");
         System.out.print(fd.exists() + "\n");
-        fd.deleteOnExit();
-        return fd;
+        fd.deleteOnExit();*/
+
+        return RiscvLivermoreELFN100iam.cholesky100;
     }
-    private File openRISCVFile() {
 
-        // static
-
-        File fd = SpecsIo.resourceCopy("org/specs/elfsimulator/riscv/test64.txt");
-
-        System.out.print(fd.exists() + "\n");
-        fd.deleteOnExit();
-        return fd;
-    }
     /*
     @Test
     void loadMicroBlazeElfTest() {
        MicroBlazeElfStream stream = new MicroBlazeElfStream(openMBFile());
        var newinst = stream.nextInstruction();
        while (newinst != null) {
-
+    
            //Print Address
            System.out.print(newinst.getAddress() + " >\t");
            
@@ -109,7 +95,7 @@ public class TestSimulator {
             //Print Generic Types
             List<InstructionType> genTypes = instData.getGenericTypes();
             //System.out.print( "\t" + genTypes + "\n");
-
+    
             newinst = stream.nextInstruction();
         }
     }
@@ -126,12 +112,12 @@ public class TestSimulator {
         //RiscvElfStream stream = new RiscvElfStream(openRISCVFile());
         InstructionSetLibGenerator islg = new InstructionSetLibGenerator(stream);
     }
-*/
+    */
     @Test
     void ElfSimulatorTest() {
-        MicroBlazeElfStream stream = new MicroBlazeElfStream(openMBFile());
-        MicroBlazeElfStream stream2 = new MicroBlazeElfStream(openMBFile());
-        //RiscvElfStream stream = new RiscvElfStream(openRISCVFile());
+        MicroBlazeElfStream stream = new MicroBlazeElfStream(MicroBlazeLivermoreELFN10.cholesky);
+        MicroBlazeElfStream stream2 = new MicroBlazeElfStream(MicroBlazeLivermoreELFN10.cholesky);
+        // RiscvElfStream stream = new RiscvElfStream(openRISCVFile());
         LibGenerator sim = new LibGenerator(stream, new InstructionSetSpecifications());
         sim.generateLib();
         ExecGenerator exec = new ExecGenerator(stream2, new InstructionSetSpecifications());
