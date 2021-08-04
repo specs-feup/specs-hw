954cfb0a6e7086c3fa13715f3d538e607aacacd1
nmcp88 <nmcp88@gmail.com>
2021-08-05 00:14:41 +0100

    [ARM] added arm elf packs to 7z and updated ArmELFProvider

--- ARM/resources/org/specs/Arm/provider/ArmELFProvider.java
+++ ARM/resources/org/specs/Arm/provider/ArmELFProvider.java
@@ -1,21 +1,21 @@
 package org.specs.Arm.provider;
 
 import org.specs.Arm.ArmApplication;
-import org.specs.Arm.ArmResource;
 
 import pt.up.fe.specs.binarytranslation.ELFProvider;
 import pt.up.fe.specs.binarytranslation.asm.Application;
-import pt.up.fe.specs.util.providers.ResourceProvider;
 
 public interface ArmELFProvider extends ELFProvider {
 
-    @Override
-    default public ResourceProvider getCPUName() {
-        return ArmResource.ARMv8_CPU_NAME;
-    }
+    final static String PREFIX = "org/specs/Arm/asm/";
 
     @Override
     default Application toApplication() {
         return new ArmApplication(this);
+    }
+
+    @Override
+    default public String getResource() {
+        return PREFIX + this.getClass().getSimpleName() + ".7z";
     }
 }
--- ARM/resources/org/specs/Arm/provider/ArmLivermoreELFN100.java
+++ ARM/resources/org/specs/Arm/provider/ArmLivermoreELFN100.java
@@ -2,28 +2,28 @@ package org.specs.Arm.provider;
 
 public enum ArmLivermoreELFN100 implements ArmELFProvider {
 
-    cholesky("org/specs/Arm/asm/N100/cholesky_N100.elf", 0x2a20, 0x2ab4),
-    diffpredict("org/specs/Arm/asm/N100/diffpredict_N100.elf", 0x1500, 0x15bc),
-    // glinearrec("org/specs/Arm/asm/N100/glinearrec_N100.elf", 0x3100, 0x31dc),
+    cholesky(0x2a20, 0x2ab4),
+    diffpredict(0x1500, 0x15bc),
+    // glinearrec(0x3100, 0x31dc),
     // linrecurrence? (there are 2 linear recurrence kernels); this one doesnt work?
-    hydro("org/specs/Arm/asm/N100/hydro_N100.elf", 0x2990, 0x2a14), // 0x2a1c),
-    hydro2d("org/specs/Arm/asm/N100/hydro2d_N100.elf", 0x2b90, 0x2e9c),
-    hydro2dimpl("org/specs/Arm/asm/N100/hydro2dimpl_N100.elf", 0x2f68, 0x3074),
-    innerprod("org/specs/Arm/asm/N100/innerprod_N100.elf", 0x1488, 0x14f4), // 0x14fc),
-    intpredict("org/specs/Arm/asm/N100/intpredict_N100.elf", 0x1e30, 0x1f7c),
-    linrec("org/specs/Arm/asm/N100/linrec_N100.elf", 0x18f0, 0x19a4),
-    matmul("org/specs/Arm/asm/N100/matmul_N100.elf", 0x2ea0, 0x2f64),
-    pic1d("org/specs/Arm/asm/N100/pic1d_N100.elf", 0x1730, 0x18ec),
-    pic2d("org/specs/Arm/asm/N100/pic2d_N100.elf", 0x15c0, 0x172c),
-    state_frag("org/specs/Arm/asm/N100/state_frag_N100.elf", 0x2ab8, 0x2b8c),
-    tri_diag("org/specs/Arm/asm/N100/tri_diag_N100.elf", 0x3078, 0x30fc);
+    hydro(0x2990, 0x2a14), // 0x2a1c),
+    hydro2d(0x2b90, 0x2e9c),
+    hydro2dimpl(0x2f68, 0x3074),
+    innerprod(0x1488, 0x14f4), // 0x14fc),
+    intpredict(0x1e30, 0x1f7c),
+    linrec(0x18f0, 0x19a4),
+    matmul(0x2ea0, 0x2f64),
+    pic1d(0x1730, 0x18ec),
+    pic2d(0x15c0, 0x172c),
+    state_frag(0x2ab8, 0x2b8c),
+    tri_diag(0x3078, 0x30fc);
 
-    private String fullPath;
+    private String elfName;
     private Number kernelStart;
     private Number kernelStop;
 
-    private ArmLivermoreELFN100(String fullPath, Number kernelStart, Number kernelStop) {
-        this.fullPath = fullPath;
+    private ArmLivermoreELFN100(Number kernelStart, Number kernelStop) {
+        this.elfName = name() + ".elf";
         this.kernelStart = kernelStart;
         this.kernelStop = kernelStop;
     }
@@ -39,7 +39,7 @@ public enum ArmLivermoreELFN100 implements ArmELFProvider {
     }
 
     @Override
-    public String getResource() {
-        return this.fullPath;
+    public String getELFName() {
+        return this.elfName;
     }
 }
