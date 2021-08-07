bfd6986a1affb5fae7be8d1cf70da9fbd80b67b3
nmcp88 <nmcp88@gmail.com>
2021-08-07 20:59:21 +0100

    [RISCV] changed RiscvApplication datastore init into static datastore

--- RISCV/src/org/specs/Riscv/RiscvApplication.java
+++ RISCV/src/org/specs/Riscv/RiscvApplication.java
@@ -1,43 +1,29 @@
 package org.specs.Riscv;
 
 import org.specs.Riscv.provider.RiscvELFProvider;
+import org.suikasoft.jOptions.DataStore.SimpleDataStore;
+import org.suikasoft.jOptions.Interfaces.DataStore;
 
 import pt.up.fe.specs.binarytranslation.asm.Application;
 
 public class RiscvApplication extends Application {
 
     private static final String ARCHPREFIX = "riscv32-unknown-elf-";
+    private static final DataStore data;
+    static {
+        data = new SimpleDataStore("RiscVApplicationDataStore");
+        data.set(CPUNAME, "riscv");
+        data.set(QEMUEXE, "qemu-system-riscv32");
+        data.set(GCC, ARCHPREFIX + "gcc");
+        data.set(GDB, ARCHPREFIX + "gdb");
+        data.set(READELF, ARCHPREFIX + "readelf");
+        data.set(OBJDUMP, ARCHPREFIX + "objdump");
+        data.set(GDBTMPLINTER, RiscvResource.GDBINTERACTIVE_TEMPLATE);
+        data.set(GDBTMPLNONINTER, RiscvResource.GDBNONINTERACTIVE_TEMPLATE);
+        data.set(QEMU_ARGS_TEMPLATE, RiscvResource.QEMU_ARGS_TEMPLATE);
+    }
 
     public RiscvApplication(RiscvELFProvider elfprovider) {
-        super(elfprovider);
-        set(CPUNAME, "riscv");
-        set(QEMUEXE, "qemu-system-riscv32");
-        set(GCC, ARCHPREFIX + "gcc");
-        set(GDB, ARCHPREFIX + "gdb");
-        set(READELF, ARCHPREFIX + "readelf");
-        set(OBJDUMP, ARCHPREFIX + "objdump");
-        set(GDBTMPLINTER, RiscvResource.GDBINTERACTIVE_TEMPLATE);
-        set(GDBTMPLNONINTER, RiscvResource.GDBNONINTERACTIVE_TEMPLATE);
-        set(QEMU_ARGS_TEMPLATE, RiscvResource.QEMU_ARGS_TEMPLATE);
-
-        /*
-         * 
-        RISCV_GCC("riscv32-unknown-elf-gcc"),
-        RISCV_GDB("riscv32-unknown-elf-gdb"),
-        RISCV_READELF("riscv32-unknown-elf-readelf"),
-        RISCV_OBJDUMP("riscv32-unknown-elf-objdump");
-        
-         */
-
-        /*
-        super(elfprovider,
-                RiscvResource.RISCV_CPU_NAME,
-                RiscvResource.RISCV_GDB,
-                RiscvResource.RISCV_OBJDUMP,
-                RiscvResource.RISCV_READELF,
-                RiscvResource.QEMU_RISCV_GDB_TEMPLATE,
-                RiscvResource.QEMU_RISCV_GDBNONINTERACTIVE_TEMPLATE,
-                RiscvResource.QEMU_RISCV_EXE,
-                null);*/
+        super(elfprovider, data);
     }
 }
