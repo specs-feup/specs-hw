1b4d05993211ba4e28fac11907d33319ecac58c6
nmcp88 <nmcp88@gmail.com>
2021-08-07 20:58:48 +0100

    [ARM] changed ArmApplication datastore into a static datastore

--- ARM/src/org/specs/Arm/ArmApplication.java
+++ ARM/src/org/specs/Arm/ArmApplication.java
@@ -1,22 +1,30 @@
 package org.specs.Arm;
 
 import org.specs.Arm.provider.ArmELFProvider;
+import org.suikasoft.jOptions.DataStore.SimpleDataStore;
+import org.suikasoft.jOptions.Interfaces.DataStore;
 
 import pt.up.fe.specs.binarytranslation.asm.Application;
 
 public class ArmApplication extends Application {
 
+    private static final String ARCHPREFIX = "aarch64-";
+    private static final DataStore data;
+    static {
+        data = new SimpleDataStore("ArmApplicationDataStore");
+        data.set(CPUNAME, "aarch64");
+        data.set(QEMUEXE, "qemu-system-aarch64");
+        data.set(GCC, ARCHPREFIX + "gcc");
+        data.set(GDB, ARCHPREFIX + "gdb");
+        data.set(READELF, ARCHPREFIX + "readelf");
+        data.set(OBJDUMP, ARCHPREFIX + "objdump");
+        data.set(GDBTMPLINTER, ArmResource.GDBINTERACTIVE_TEMPLATE);
+        data.set(GDBTMPLNONINTER, ArmResource.GDBNONINTERACTIVE_TEMPLATE);
+        data.set(BAREMETAL_DTB, ArmResource.BAREMETAL_DTB);
+        data.set(QEMU_ARGS_TEMPLATE, ArmResource.QEMU_ARGS_TEMPLATE);
+    }
+
     public ArmApplication(ArmELFProvider elfprovider) {
-        super(elfprovider);
-        set(CPUNAME, "aarch64");
-        set(QEMUEXE, "qemu-system-aarch64");
-        set(GCC, "aarch64-none-elf-gcc");
-        set(GDB, "aarch64-none-elf-gdb");
-        set(READELF, "aarch64-none-elf-readelf");
-        set(OBJDUMP, "aarch64-none-elf-objdump");
-        set(GDBTMPLINTER, ArmResource.GDBINTERACTIVE_TEMPLATE);
-        set(GDBTMPLNONINTER, ArmResource.GDBNONINTERACTIVE_TEMPLATE);
-        set(BAREMETAL_DTB, ArmResource.QEMU_AARCH64_BAREMETAL_DTB);
-        set(QEMU_ARGS_TEMPLATE, ArmResource.QEMU_ARGS_TEMPLATE);
+        super(elfprovider, data);
     }
 }
