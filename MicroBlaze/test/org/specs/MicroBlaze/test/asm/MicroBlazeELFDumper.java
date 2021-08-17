c4717d45e72a110072e22398bac9dda4e8c360ba
Nuno <nmcp88@gmail.com>
2021-08-17 19:31:20 +0100

    [MicroBlaze] tester tweaks

--- MicroBlaze/test/org/specs/MicroBlaze/test/asm/MicroBlazeELFDumper.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/asm/MicroBlazeELFDumper.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Test;
-import org.specs.MicroBlaze.provider.MicroBlazeLivermoreN10;
+import org.specs.MicroBlaze.provider.MicroBlazeLivermoreN100;
+import org.specs.MicroBlaze.provider.MicroBlazePolyBenchMiniFloat;
 
 import pt.up.fe.specs.binarytranslation.ELFProvider;
 import pt.up.fe.specs.binarytranslation.test.asm.ELFDumperTester;
@@ -14,14 +15,16 @@ public class MicroBlazeELFDumper extends ELFDumperTester {
     @Test
     public void sequentialDump() {
         var test = new ArrayList<ELFProvider>();
-        test.addAll((Arrays.asList(MicroBlazeLivermoreN10.values())));
+        // test.addAll((Arrays.asList(MicroBlazeLivermoreN100.values())));
+        // test.addAll((Arrays.asList(MicroBlazePolyBenchMiniInt.values())));
+        test.addAll((Arrays.asList(MicroBlazePolyBenchMiniFloat.values())));
         sequentialDump(test);
     }
 
     @Test
     public void parallelDump() {
         var test = new ArrayList<ELFProvider>();
-        test.addAll((Arrays.asList(MicroBlazeLivermoreN10.values())));
+        test.addAll((Arrays.asList(MicroBlazeLivermoreN100.values())));
         parallelDump(test);
     }
 }
