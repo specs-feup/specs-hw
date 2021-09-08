1d719703523c703239f9730d25ca421b3c488063
Nuno <nmcp88@gmail.com>
2021-09-08 14:48:00 +0100

    [MicroBlaze] cleaned up InstructionData copy methods

--- MicroBlaze/src/org/specs/MicroBlaze/instruction/MicroBlazeInstructionData.java
+++ MicroBlaze/src/org/specs/MicroBlaze/instruction/MicroBlazeInstructionData.java
@@ -1,14 +1,9 @@
 package org.specs.MicroBlaze.instruction;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.specs.MicroBlaze.parsing.MicroBlazeAsmFieldData;
 
 import pt.up.fe.specs.binarytranslation.instruction.InstructionData;
 import pt.up.fe.specs.binarytranslation.instruction.InstructionProperties;
-import pt.up.fe.specs.binarytranslation.instruction.InstructionType;
-import pt.up.fe.specs.binarytranslation.instruction.operand.Operand;
 
 public class MicroBlazeInstructionData extends InstructionData {
 
@@ -22,23 +17,33 @@ public class MicroBlazeInstructionData extends InstructionData {
 
     /*
      * Helper constructor for copy, calls super copy
-     */
+     
     private MicroBlazeInstructionData(String plainname,
             int latency, int delay, List<InstructionType> genericType, List<Operand> ops) {
         super(plainname, latency, delay, genericType, ops);
+    }*/
+
+    /*
+     * Helper constructor for copy, calls super copy
+     */
+    private MicroBlazeInstructionData(MicroBlazeInstructionData other) {
+        super(other);
     }
 
     /*
      * Copy "constructor"
      */
     public MicroBlazeInstructionData copy() {
-        String copyname = new String(this.plainname);
+        return new MicroBlazeInstructionData(this);
+        /*String copyname = new String(this.plainname);
         List<InstructionType> copytype = new ArrayList<InstructionType>(this.genericType);
-
+        
         List<Operand> copyops = new ArrayList<Operand>();
         for (Operand op : this.operands)
             copyops.add(op.copy());
-
+        
         return new MicroBlazeInstructionData(copyname, this.latency, this.delay, copytype, copyops);
+        */
+
     }
 }
