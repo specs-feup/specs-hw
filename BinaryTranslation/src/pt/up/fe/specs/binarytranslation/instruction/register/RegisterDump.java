4484e2e5de134458f604abefbc3c1b015daca799
Nuno <nmcp88@gmail.com>
2021-09-10 18:45:12 +0100

    [BinaryTranslation] new classes to hold ExecutedRegister/Immediate vals

--- BinaryTranslation/src/pt/up/fe/specs/binarytranslation/instruction/register/RegisterDump.java
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/instruction/register/RegisterDump.java
@@ -5,9 +5,14 @@ import java.util.Map;
 public interface RegisterDump {
 
     /*
-     * 
+     * get value by register definition (i.e. enum), such as MicroBlazeRegister.R1
      */
     public Number getValue(Register registerName);
+
+    /*
+     * get value by register string name, such as "r1"
+     */
+    // public Number getValue(String registerName);
 
     /*
      * 
---
+++ BinaryTranslation/src/pt/up/fe/specs/binarytranslation/instruction/register/RegisterResolver.java
@@ -0,0 +1,5 @@
+package pt.up.fe.specs.binarytranslation.instruction.register;
+
+interface RegisterResolver {
+
+}
