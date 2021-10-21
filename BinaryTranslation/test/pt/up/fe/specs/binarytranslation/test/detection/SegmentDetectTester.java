304140d539869c2fa8a643e7e4282e654a37297e
Nuno <nmcp88@gmail.com>
2021-10-21 10:28:47 +0100

    [BinaryTranslation] minor comment and tester changes abou4 g4 and lex

--- BinaryTranslation/test/pt/up/fe/specs/binarytranslation/test/detection/SegmentDetectTester.java
+++ BinaryTranslation/test/pt/up/fe/specs/binarytranslation/test/detection/SegmentDetectTester.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
  */
- 
+
 package pt.up.fe.specs.binarytranslation.test.detection;
 
 import pt.up.fe.specs.binarytranslation.detection.detectors.DetectorConfiguration.DetectorConfigurationBuilder;
