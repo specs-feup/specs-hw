c133aef80ca6d4610bb325cff20179cd8c1003e1
Nuno <nmcp88@gmail.com>
2021-11-26 15:54:26 +0000

    [MicroBlaze] Removed old Rosetta 7z; added DTB for 4GB Microblaze syst

--- MicroBlaze/test/org/specs/MicroBlaze/test/generators/MicroBlazeSingleInstructionModuleGeneratorTester.java
+++ MicroBlaze/test/org/specs/MicroBlaze/test/generators/MicroBlazeSingleInstructionModuleGeneratorTester.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
  */
- 
+
 package org.specs.MicroBlaze.test.generators;
 
 import org.junit.Test;
@@ -26,7 +26,7 @@ public class MicroBlazeSingleInstructionModuleGeneratorTester {
         var addi = MicroBlazeInstruction.newInstance("248", "20c065e8");
         var singleUnitBuilder = new SingleInstructionModuleGenerator();
         var unit = singleUnitBuilder.generateHarware(addi);
-        // unit.emit(System.out);
+        unit.emit(System.out);
 
         // new dotty generation test
         // var dottygen = new DottyGenerator<HardwareNode>();
