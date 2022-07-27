de44b11f67fd8d6bf5c9bae44f27b8d6fa9768db
1181626 <1181626@isep.ipp.pt>
2022-07-27 22:55:41 +0100

    Development in the Java project

--- F4PGAAPI/src/pt/up/fe/f4pga/Main.java
+++ F4PGAAPI/src/pt/up/fe/f4pga/Main.java
@@ -1,6 +1,7 @@
 package pt.up.fe.f4pga;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import pt.up.fe.specs.util.SpecsIo;
@@ -10,6 +11,20 @@ import pt.up.fe.specs.util.utilities.Replacer;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		F4PGA a = new F4PGA();
+		try {
+			a.init("eos-s3", "hw");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		FPGA object = new FPGA("","");
+		
+		ArrayList<FPGA> list = object.getListValidFPGA();
+		
+		
 		System.out.println("aqui");
 		String i = SpecsIo.getResource("pt/up/fe/f4pga/makefile");
 		//System.out.println(i);
@@ -21,13 +36,7 @@ public class Main {
 		//var processOutput = SpecsSystem.runProcess(Arrays.asList("dir"), true, false);
 		//System.out.println(processOutput.getOutput());
 		//String i = SpecsIo.getResource("resourceExample");
-		F4PGA a = new F4PGA();
-		try {
-			a.init("eos-s3");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 
 }
