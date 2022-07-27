84274551de150a7e88a76b2028dfea2d8cc334a9
1181626 <1181626@isep.ipp.pt>
2022-07-27 18:30:49 +0100

    Developments in Main.java. Class F4PGA.java and started

--- F4PGAAPI/src/pt/up/fe/f4pga/Main.java
+++ F4PGAAPI/src/pt/up/fe/f4pga/Main.java
@@ -1,6 +1,10 @@
 package pt.up.fe.f4pga;
 
+import java.io.File;
+import java.util.Arrays;
+
 import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsSystem;
 import pt.up.fe.specs.util.utilities.Replacer;
 
 public class Main {
@@ -8,13 +12,22 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("aqui");
 		String i = SpecsIo.getResource("pt/up/fe/f4pga/makefile");
-		System.out.println(i);
+		//System.out.println(i);
 		//Replacer r = new Replacer(() -> "pt/up/fe/f4pga/makefile");
 		Replacer r = new Replacer(i);
 		r.replace("basys3", "Antonio");
 		r.replace("<put the name of your top module here>", "MODIFICADO");
-		r.toString();
+		//System.out.println("Replaced:\n"+r.toString());
+		//var processOutput = SpecsSystem.runProcess(Arrays.asList("dir"), true, false);
+		//System.out.println(processOutput.getOutput());
 		//String i = SpecsIo.getResource("resourceExample");
+		F4PGA a = new F4PGA();
+		try {
+			a.init("eos-s3");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
