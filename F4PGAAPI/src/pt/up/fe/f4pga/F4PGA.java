9f1159a4b66efdd08264089b5250f5bcb7efd2cf
ManelMCCS <up201806391@fe.up.pt>
2022-07-28 14:28:39 +0100

    implemented build, not tested

--- F4PGAAPI/src/pt/up/fe/f4pga/F4PGA.java
+++ F4PGAAPI/src/pt/up/fe/f4pga/F4PGA.java
@@ -1,6 +1,5 @@
 package pt.up.fe.f4pga;
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,59 +9,59 @@ import pt.up.fe.specs.util.SpecsSystem;
 import pt.up.fe.specs.util.utilities.Replacer;
 
 public class F4PGA {
-	
+
 	FPGA validFPGA;
-	
+
 	final ArrayList<FPGA> list = new ArrayList<>();
-	
-	
+
 	public void init(String fpgaFamilyName, String fpgaHwType) throws Exception {
-		
-		//Checking if FPGA is supported by F4PGA
+
+		// Checking if FPGA is supported by F4PGA
 		boolean aux = isFpgaValid(fpgaFamilyName, fpgaHwType);
-		
-		if(aux == false) {
+
+		if (aux == false) {
 			throw new Exception("Invalid FPGA Family Name");
 		}
-		
-		//Saving FPGA in a class variable
-		 validFPGA = new FPGA(fpgaFamilyName, fpgaHwType);
-		
-		//getting the resource (f4pga_config.sh)
+
+		// Saving FPGA in a class variable
+		validFPGA = new FPGA(fpgaFamilyName, fpgaHwType);
+
+		// getting the resource (f4pga_config.sh)
 		String i = SpecsIo.getResource("pt/up/fe/f4pga/f4pga_config.sh");
-		
-		//Creating a replacer
+
+		// Creating a replacer
 		Replacer r = new Replacer(i);
-		
+
 		r.replace("<FPGA_FAM>", fpgaFamilyName);
-		
-		System.out.println(i);		
-		
+
+		System.out.println(i);
+
 		var processOutput = SpecsSystem.runProcess(Arrays.asList("export F4PGA_INSTALL_DIR=~/opt/f4pga"), true, false);
-		
-	//	FPGA_FAM="xc7"
-		//		var processOutput = SpecsSystem.runProcess(Arrays.asList("FPGA_FAM=\"xc7\""), true, false);
+
+		// FPGA_FAM="xc7"
+		// var processOutput = SpecsSystem.runProcess(Arrays.asList("FPGA_FAM=\"xc7\""),
+		// true, false);
 	}
-	
-	
-	public boolean isFpgaValid(String fpgaFamilyName, String fpgaHwType ) {
-		
+
+	public boolean isFpgaValid(String fpgaFamilyName, String fpgaHwType) {
+
 		boolean aux = false;
-		
+
 		for (int i = 0; i < list.size(); i++) {
-			if((list.get(i).getFpgaFamily() == fpgaFamilyName) && (list.get(i).getHwType() == fpgaHwType)) {
+			if ((list.get(i).getFpgaFamily() == fpgaFamilyName) && (list.get(i).getHwType() == fpgaHwType)) {
 				aux = true;
 			}
 		}
 		return aux;
-		
+
 	}
-	
+
+	public void build(String buildDir) {
+		String target = validFPGA.getHwType;
+
+		String buildCmd = "TARGET=/" + target + " make -C " + buildDir;
+		var processOutput = SpecsSystem.runProcess(Arrays.asList(buildCmd), true, false);
+
+	}
 
 }
-
-
-
-
-
-
