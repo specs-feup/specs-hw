1dbac4e4b7d12990203b496f31b2c14e731c4cc3
ManelMCCS <up201806391@fe.up.pt>
2022-07-30 17:35:52 +0100

    fpga model input working

--- F4PGAAPI/src/pt/up/fe/f4pga/FPGA.java
+++ F4PGAAPI/src/pt/up/fe/f4pga/FPGA.java
@@ -2,28 +2,21 @@ package pt.up.fe.f4pga;
 
 
 public class FPGA {
-	
+
 	String fpgaFamily;
-	
 	String hwType;
 	
 	public FPGA(String hwType , String fpgaFamily) {
 		this.fpgaFamily=fpgaFamily;
 		this.hwType = hwType;
 	}
-	
-	
+
 	public String getFpgaFamily() {
 		return fpgaFamily;
 	}
-	
+
 	public String getHwType() {
 		return hwType;
 	}
-	
-	
-	
-	
-	
 
 }
